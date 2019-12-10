394fb1c5194e3025a10e8ea82147cc59b5fdb00a
Emanuele Galli <egalli64@gmail.com>
2019-12-10 11:13:48 +0100

    concatenation of string and boolean

--- src/main/java/s036/Concatenation.java
+++ src/main/java/s036/Concatenation.java
@@ -4,6 +4,7 @@ public class Concatenation {
     public static void main(String[] args) {
         System.out.println("Resistence" + " is " + "useless");
         System.out.println("Solution: " + 42);
+        System.out.println(true + " or " + false);
 
         System.out.println("Vogons".repeat(3)); // Java 11
     }
