485c7d1bb72cff94d289d389fcc33214dc81b835
Manny <egalli64@gmail.com>
2020-04-09 11:29:47 +0200

    m2

---
+++ src/main/java/m2/s36/Adder.java
@@ -0,0 +1,23 @@
+package m2.s36;
+
+import java.util.Scanner;
+
+public class Adder {
+    public static void main(String[] args) {
+        System.out.println("Please, enter a few numbers");
+        double result = 0;
+
+        // try-with-resources
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (scanner.hasNext()) {
+                if (scanner.hasNextDouble()) {
+                    result += scanner.nextDouble();
+                } else {
+                    System.out.println("Bad input, discarded: " + scanner.next());
+                }
+            }
+        }
+
+        System.out.println("Total is " + result);
+    }
+}
