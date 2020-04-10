b3fdea9be975879293c26d0a279b3e60d3f95438
Manny <egalli64@gmail.com>
2020-04-10 17:33:50 +0200

    module 5

---
+++ src/main/java/m5/s08/AsSortedSet.java
@@ -0,0 +1,20 @@
+package m5.s08;
+
+import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class AsSortedSet {
+    public static void main(String[] args) {
+        SortedSet<Integer> sortedSet = aSortedSet();
+        System.out.println(sortedSet);
+        System.out.println("First: " + sortedSet.first());
+        System.out.println("Last: " + sortedSet.last());
+        System.out.println(sortedSet.subSet(sortedSet.first() + 1, sortedSet.last()));
+    }
+
+    private static SortedSet<Integer> aSortedSet() {
+        return new TreeSet<Integer>(Arrays.asList(12, 18, -5, -2233));
+    }
+
+}
---
+++ src/main/java/m5/s14/ArrayListUser.java
@@ -0,0 +1,11 @@
+package m5.s14;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListUser {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>(Arrays.asList(23, 12, 18, 5));
+        System.out.println(al);
+    }
+}
