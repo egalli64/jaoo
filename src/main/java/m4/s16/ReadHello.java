29e202e19cdc579b1714ef087d638548bf2253ec
Manny <egalli64@gmail.com>
2020-04-19 20:45:03 +0200

    tab

--- src/main/java/m4/s16/ReadHello.java
+++ src/main/java/m4/s16/ReadHello.java
@@ -6,18 +6,18 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class ReadHello {
-	public static void main(String[] args) {
-		File f = new File("/tmp/hello.txt");
+    public static void main(String[] args) {
+        File f = new File("/tmp/hello.txt");
 
-		BufferedReader br;
-		try {
-			br = new BufferedReader(new FileReader(f));
-			String line = br.readLine();
-			System.out.println(">" + line + "<");
+        BufferedReader br;
+        try {
+            br = new BufferedReader(new FileReader(f));
+            String line = br.readLine();
+            System.out.println(">" + line + "<");
 
-			br.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
