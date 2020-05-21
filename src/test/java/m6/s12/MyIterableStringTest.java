d9a644c6db93e32e6255c16b890318340014cfb8
Manny <egalli64@gmail.com>
2020-05-21 10:26:24 +0200

    import cleanup

--- src/test/java/m6/s12/MyIterableStringTest.java
+++ src/test/java/m6/s12/MyIterableStringTest.java
@@ -1,14 +1,12 @@
 package m6.s12;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-
-import org.junit.jupiter.api.Test;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 class MyIterableStringTest {
     @Test
