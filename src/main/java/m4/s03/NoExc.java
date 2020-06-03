9148d97fee9b1877c9c7a9e2a3563648d6df8a4d
Manny <egalli64@gmail.com>
2020-06-03 14:45:32 +0200

    more example on exceptions

---
+++ src/main/java/m4/s03/NoExc.java
@@ -0,0 +1,40 @@
+package m4.s03;
+
+public class NoExc {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        try {
+            System.out.println(increment(Integer.MAX_VALUE));
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        int y = 3;
+        
+        // ...
+        
+        try {
+            y = increment(Integer.MAX_VALUE);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        // ...
+        
+        System.out.println(y);
+    }
+
+    /**
+     * 
+     * @param x
+     * @return
+     */
+    static int increment(int x) throws Exception {
+        if(x == Integer.MAX_VALUE) {
+            throw new Exception();
+        }
+        return x + 1;
+    }
+}
