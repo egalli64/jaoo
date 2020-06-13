d84a2a6f81ca448bcf9faf8ab3d8895ef407d893
Manny <egalli64@gmail.com>
2020-06-13 14:59:01 +0200

    variations on example

--- src/main/java/m6/s12/MyIterableString.java
+++ src/main/java/m6/s12/MyIterableString.java
@@ -12,6 +12,8 @@ public class MyIterableString implements Iterable<Character> {
 
     @Override
     public Iterator<Character> iterator() {
+//        return new MyIterator(s);
+
         return new Iterator<Character>() {
             private int pos = 0;
 
@@ -23,11 +25,53 @@ public class MyIterableString implements Iterable<Character> {
             @Override
             public Character next() {
                 try {
-                    return s.charAt(pos++);
+                    char c = s.charAt(pos);
+                    pos += 1;
+                    return c;
+//                    return s.charAt(pos++);
                 } catch (Exception e) {
                     throw new NoSuchElementException(e.getMessage());
                 }
             }
         };
+    }
+}
+
+class X {
+    public static void main(String[] args) {
+        MyIterableString mis = new MyIterableString("ciao");
+
+        Iterator<Character> it = mis.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        for (Character c : mis) {
+            System.out.println(c);
+        }
+
+    }
+}
+
+class MyIterator implements Iterator<Character> {
+    private String s;
+    private int pos = 0;
+
+    public MyIterator(String s) {
+        this.s = s;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return s.length() > pos;
+    }
+
+    @Override
+    public Character next() {
+        try {
+            return s.charAt(pos++);
+        } catch (Exception e) {
+            throw new NoSuchElementException(e.getMessage());
+        }
     }
 }
