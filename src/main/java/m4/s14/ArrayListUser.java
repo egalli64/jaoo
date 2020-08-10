ed6cd41542e53155f15295efb8040c097e11e7cb
Manny <egalli64@gmail.com>
2020-08-10 14:59:00 +0200

    using JUL

--- src/main/java/m4/s14/ArrayListUser.java
+++ src/main/java/m4/s14/ArrayListUser.java
@@ -2,10 +2,23 @@ package m4.s14;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.logging.Logger;
 
 public class ArrayListUser {
-    public static void main(String[] args) {
-        ArrayList<Integer> al = new ArrayList<>(Arrays.asList(23, 12, 18, 5));
-        System.out.println(al);
-    }
+	private static final Logger LOG = Logger.getLogger("collections");
+
+	public static void main(String[] args) {
+		ArrayList<Integer> al = new ArrayList<>(Arrays.asList(23, 12, 18, 5));
+		LOG.info("Values in al: " + al);
+
+		ArrayList<Integer> al2 = new ArrayList<Integer>();
+		al2.add(56);
+
+		// ...
+
+		Integer value = al2.get(0);
+		LOG.info("Value in al2[0]: " + value);
+		
+
+	}
 }
