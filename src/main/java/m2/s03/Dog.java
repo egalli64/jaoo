a35f1c2b1b342c0abbfaaba0db10252574a81854
Manny <egalli64@gmail.com>
2020-08-10 15:04:52 +0200

    static example added

--- src/main/java/m2/s03/Dog.java
+++ src/main/java/m2/s03/Dog.java
@@ -2,16 +2,19 @@ package m2.s03;
 
 public class Dog {
     String name;
+    static int counter = 0;
 
     public Dog() {
         name = "No name";
-        System.out.println("A new anonymous dog has been created");
+        counter += 1;
+        //System.out.println("A new anonymous dog has been created");
     }
 
     // not a good parameter name, see "this"
     public Dog(String nm) {
+        counter += 1;
         name = nm;
-        System.out.println("A new dog named " + name + " has been created");
+        //System.out.println("A new dog named " + name + " has been created");
     }
 
     public void bark() {
