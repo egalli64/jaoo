9018c14ae1ec564c49a9b3e470f36caec19518ba
Manny <egalli64@gmail.com>
2020-08-12 09:38:57 +0200

    A sample exception

---
+++ src/main/java/m1ex/ExerciseException.java
@@ -0,0 +1,9 @@
+package m1ex;
+
+public class ExerciseException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public ExerciseException(String message) {
+		super(message);
+	}
+}
