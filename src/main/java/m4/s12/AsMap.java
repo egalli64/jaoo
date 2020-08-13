1f5e0a6bb82b8f3feffc37c09953eddf62262e98
Manny <egalli64@gmail.com>
2020-08-13 17:17:11 +0200

    code cleanup

--- src/main/java/m4/s12/AsMap.java
+++ src/main/java/m4/s12/AsMap.java
@@ -8,30 +8,42 @@ import java.util.Set;
 public class AsMap {
     public static void main(String[] args) {
         Map<Integer, String> map = aMap();
+        System.out.println("original map: " + map);
+
+        // Set<Map.Entry<Integer, String>>
+        var items = map.entrySet();
+        // Map.Entry<Integer, String>
+        for (var item : items) {
+            item.setValue(item.getValue() + "!");
+        }
+        System.out.println("modified map: " + map);
+
+        Set<Integer> keys = map.keySet();
+        System.out.print("accessing map from keys " + keys + ": ");
+        for (Integer key : keys) {
+            System.out.print(String.format("[%d: %s] ", key, map.get(key)));
+        }
+        System.out.println();
+
+        // Iterator<Map.Entry<Integer, String>>
+        var it = items.iterator();
+        System.out.print("looping via map iterator: ");
+        while (it.hasNext()) {
+            // Map.Entry<Integer, String>
+            var item = it.next();
+            System.out.print(String.format("[%d, %s] ", item.getKey(), item.getValue()));
+        }
+        System.out.println();
+
+        System.out.println("values are: " + map.values());
 
         System.out.println("contains key 12: " + map.containsKey(12));
         System.out.println("contains value seven: " + map.containsValue("seven"));
 
-        Set<Map.Entry<Integer, String>> items = map.entrySet();
-        for (Map.Entry<Integer, String> item : items) {
-            item.setValue(item.getValue() + "!");
-        }
-
-        Iterator<Map.Entry<Integer, String>> it = items.iterator();
-        while (it.hasNext()) {
-            Map.Entry<Integer, String> item = it.next();
-            System.out.println("Key: " + item.getKey() + ", value: " + item.getValue());
-        }
+        System.out.println("is the map empty? " + map.isEmpty());
 
         System.out.println("Value for key -1 (or null): " + map.get(-1));
         System.out.println("Value for key -1 (or default): " + map.getOrDefault(-1, "missing"));
-
-        Set<Integer> keys = map.keySet();
-        for (Integer key : keys) {
-            System.out.println(key + ": " + map.get(key));
-        }
-
-        System.out.println("Keys are: " + keys);
 
         System.out.println("Previous value for 99 was: " + map.put(99, "nintynine"));
         System.out.println("Current value for 99 is: " + map.putIfAbsent(99, "star"));
@@ -40,10 +52,8 @@ public class AsMap {
         System.out.println("Replaced value for key 12: " + map.replace(12, "twelve"));
 
         System.out.println("Size is " + map.size());
-        System.out.println("Values are: " + map.values());
 
         map.clear();
-        System.out.println("Is the map empty? " + map.isEmpty());
     }
 
     private static Map<Integer, String> aMap() {
