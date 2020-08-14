5a23c284dffe33db294a3db99f2fe1a637d47cb3
Manny <egalli64@gmail.com>
2020-08-14 17:47:46 +0200

    cleanup

--- src/main/java/m5/s12/MyIterableString.java
+++ src/main/java/m5/s12/MyIterableString.java
@@ -37,22 +37,6 @@ public class MyIterableString implements Iterable<Character> {
     }
 }
 
-class X {
-    public static void main(String[] args) {
-        MyIterableString mis = new MyIterableString("ciao");
-
-        Iterator<Character> it = mis.iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
-
-        for (Character c : mis) {
-            System.out.println(c);
-        }
-
-    }
-}
-
 class MyIterator implements Iterator<Character> {
     private String s;
     private int pos = 0;
