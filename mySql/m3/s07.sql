66016e75482e3feb21fd0ffc756a800b093a2e79
Manny <egalli64@gmail.com>
2020-08-19 07:59:23 +0200

    alternative regexp

--- mySql/m3/s07.sql
+++ mySql/m3/s07.sql
@@ -9,3 +9,15 @@ where regexp_like(last_name, '^[ae].*');
 select last_name
 from employees
 where regexp_like(last_name, '.*[nt]$');
+
+select last_name
+from employees
+where last_name regexp '^[ae].*';
+
+select last_name
+from employees
+where last_name regexp '.*[nt]$';
+
+select last_name
+from employees
+where last_name rlike '.*[nt]$';
