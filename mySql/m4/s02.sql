6525c8c73da15b8b18017029379474c69674538a
Manny <egalli64@gmail.com>
2020-08-19 08:01:39 +0200

    multiline insert

--- mySql/m4/s02.sql
+++ mySql/m4/s02.sql
@@ -4,11 +4,16 @@ use me;
 select * from regions;
 
 -- plain insert
-insert into regions(region_id, region_name)
+insert into regions (region_id, region_name)
 values (11, 'Antarctica');
 
+-- multiline insert
+insert into regions (region_id, region_name) values
+	(21, 'Gondor'),
+	(22, 'Rohan');
+
 -- nullable column could be skipped
-insert into regions(region_id)
+insert into regions (region_id)
 values (12);
 
 -- insert relying on column definition on table
