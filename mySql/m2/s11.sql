351f4f5c75c8d7943e75e1c78577976cd24cd19a
Manny <egalli64@gmail.com>
2020-08-29 08:47:29 +0200

    more sensible example

--- mySql/m2/s11.sql
+++ mySql/m2/s11.sql
@@ -1,13 +1,21 @@
 use me;
 
 -- non-equi join
-select e.last_name, e.salary, j.min_salary
+
+-- given an employee ...
+select first_name, last_name, salary, job_title, min_salary, max_salary
 from employees e join jobs j
-on(e.salary between j.min_salary and j.min_salary + 100)
-where(e.job_id = j.job_id);
+using (job_id)
+where employee_id = 107;
+
+-- ... list of other jobs with a compatible  min/max salary
+select j.job_title, j.min_salary, j.max_salary
+from employees e join jobs j
+on(e.salary between j.min_salary and j.max_salary and e.job_id != j.job_id)
+where e.employee_id = 107;
 
 -- "classic" version
-select e.last_name, e.salary, j.min_salary
+select j.job_title, j.min_salary, j.max_salary
 from employees e, jobs j
-where e.salary between j.min_salary and j.min_salary + 100
-and e.job_id = j.job_id;
+where e.salary between j.min_salary and j.max_salary
+	and e.job_id != j.job_id and e.employee_id = 107;
