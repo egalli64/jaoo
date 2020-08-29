4a895d481afeb852bab6f361f163eecd9d990f09
Manny <egalli64@gmail.com>
2020-08-29 10:41:34 +0200

    more details

--- mySql/m4/s04.sql
+++ mySql/m4/s04.sql
@@ -1,8 +1,16 @@
 -- delete
 use me;
 
+-- check the current status
 select * from regions;
 
--- delete regions
+-- create a test row
+insert into regions (region_id, region_name) values (42, 'Z');
+
+-- delete a single row
+delete from regions
+where region_id = 42;
+
+-- multiline delete - be careful with it!
 delete from regions
 where region_id > 10;
