1bf12d87ce46fbbe878fb76f372f6634c50d10f5
Manny <egalli64@gmail.com>
2020-08-29 12:08:21 +0200

    DML TCL added

--- mySql/m4/s08.sql
+++ mySql/m4/s08.sql
@@ -6,3 +6,20 @@ create table items (
     status char,
     name varchar(20),
     coder_id integer);
+
+select * from items;
+
+insert into items (item_id) values (12);
+
+update items
+set status = 'x'
+where item_id = 12;
+
+savepoint sp;
+
+delete from items;
+
+rollback to sp;
+
+rollback;
+
