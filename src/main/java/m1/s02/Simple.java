65eed70192d465f02780bf3b7a75f072375c26bf
Manny <egalli64@gmail.com>
2020-09-21 17:08:22 +0200

    another comment

--- src/main/java/m1/s02/Simple.java
+++ src/main/java/m1/s02/Simple.java
@@ -10,6 +10,7 @@ import java.lang.Math; // not required
  */
 public class Simple {
     public static void main(String[] args) {
+        // a comment
         System.out.println(Math.PI);
     }
 }
