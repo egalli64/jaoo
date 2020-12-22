96abf0731d55ac25d0262a04b8c5d5bd212a5f05
Manny <egalli64@gmail.com>
2020-12-22 12:01:57 +0100

    more tests

--- src/test/java/mex/s05/Exercise6bTest.java
+++ src/test/java/mex/s05/Exercise6bTest.java
@@ -13,4 +13,36 @@ class Exercise6bTest {
 
         assertThat(actual, is(expected));
     }
+
+    @Test
+    void binarySumVerySimple() {
+        String expected = "11";
+        String actual = Exercise6b.binAdd("10", "01");
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    void binarySumSimple() {
+        String expected = "101";
+        String actual = Exercise6b.binAdd("10", "11");
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    void binarySum15x2() {
+        String expected = "11110";
+        String actual = Exercise6b.binAdd("1111", "1111");
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    void binarySum15plus1() {
+        String expected = "10000";
+        String actual = Exercise6b.binAdd("1111", "0001");
+
+        assertThat(actual, is(expected));
+    }
 }
