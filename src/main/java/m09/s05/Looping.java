07dd253d8fdac7180cd45afd9826e4afcde7844a
Manny <egalli64@gmail.com>
2021-01-19 11:57:09 +0100

    minor changes

--- src/main/java/m09/s05/Looping.java
+++ src/main/java/m09/s05/Looping.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
-public class Iterating {
+public class Looping {
     public static void main(String[] args) {
         List<Integer> aList = Arrays.asList(1, 3, 4, 2);
         Iterable<Integer> iterable = new ArrayList<Integer>(aList);
@@ -23,12 +23,12 @@ public class Iterating {
         }
         System.out.println();
 
-        // Java8 forEach
-        iterable.forEach(System.out::print);
-        System.out.println();
-
         // Java8 forEach w/ blanks
         iterable.forEach(value -> System.out.print(value + " "));
+        System.out.println();
+
+        // Java8 forEach
+        iterable.forEach(System.out::print);
         System.out.println();
     }
 }
