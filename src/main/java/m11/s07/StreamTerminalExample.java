54894d26b288f6b521ef10333343cb37ea0d84aa
Manny <egalli64@gmail.com>
2021-01-20 21:50:00 +0100

    Stream terminal operations

---
+++ src/main/java/m11/s07/StreamTerminalExample.java
@@ -0,0 +1,20 @@
+package m11.s07;
+
+import static java.util.stream.Collectors.toSet;
+
+import java.util.List;
+
+public class StreamTerminalExample {
+    public static void main(String[] args) {
+        List<Integer> values = List.of(11, 34, 2, 11, 5, 2);
+        System.out.println("Original data: " + values);
+
+        System.out.println("count(): " + values.stream().count());
+
+        System.out.println("forEach() begin");
+        values.stream().forEach(System.out::println);
+        System.out.println("forEach() end");
+
+        System.out.println("collect(toSet): " + values.stream().collect(toSet()));
+    }
+}
