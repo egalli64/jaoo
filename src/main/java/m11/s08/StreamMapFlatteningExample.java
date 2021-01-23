59a3b5a516e22fa5358c8d298e96ea0f5dbbe843
Manny <egalli64@gmail.com>
2021-01-23 12:10:05 +0100

    readability

--- src/main/java/m11/s08/StreamMapFlatteningExample.java
+++ src/main/java/m11/s08/StreamMapFlatteningExample.java
@@ -2,18 +2,35 @@ package m11.s08;
 
 import java.util.Arrays;
 import java.util.List;
+import static java.util.stream.Collectors.toList;
 
 public class StreamMapFlatteningExample {
     public static void main(String[] args) {
         List<Integer> li1 = List.of(4, 45, 49, 18);
         List<Integer> li2 = List.of(14, 5, 9, 81);
 
-        // all the items, sorted, in the two lists
         var lists = new List[] { li1, li2 };
-        Arrays.stream(lists).flatMap(List<Integer>::stream).sorted().forEach(System.out::println);
+        var lix = Arrays.stream(lists).flatMap(List<Integer>::stream).sorted().collect(toList());
+        System.out.println("All items, sorted: " + lix);
 
-        // cartesian product
-        li1.stream().sorted().flatMap(i -> li2.stream().sorted().map(j -> new Integer[] { i, j })) //
-                .forEach(x -> System.out.println(Arrays.toString(x)));
+        class Pair {
+            Integer first;
+            Integer second;
+
+            public Pair(Integer first, Integer second) {
+                this.first = first;
+                this.second = second;
+            }
+
+            @Override
+            public String toString() {
+                return "[" + first + ", " + second + "]";
+            }
+        }
+
+        var cartesian = li1.stream().sorted() //
+                .flatMap(i -> li2.stream().sorted().map(j -> new Pair(i, j))) //
+                .collect(toList());
+        System.out.println("Cartesian product: " + cartesian);
     }
 }
