8ed40273a33cf56126bdde4d3dcf5c3e93bbeb50
Manny <egalli64@gmail.com>
2021-01-24 20:54:25 +0100

    iterate / generate

---
+++ src/main/java/m11/s12/UnboundedStreamExample.java
@@ -0,0 +1,14 @@
+package m11.s12;
+
+import static java.util.stream.Collectors.toList;
+import java.util.stream.Stream;
+
+public class UnboundedStreamExample {
+    public static void main(String[] args) {
+        var evenNumbers = Stream.iterate(2, x -> x + 2).limit(5).collect(toList());
+        System.out.println("The first five even numbers: " + evenNumbers);
+
+        var randomNumbers = Stream.generate(Math::random).limit(5).collect(toList());
+        System.out.println("Five random numbers in [0, 1): " + randomNumbers);
+    }
+}
