b04939667bcba4fc08b47f7eec822d85018bc80c
Emanuele Galli <egalli64@gmail.com>
2021-02-06 15:18:55 +0100

    improved description

--- src/main/java/m09/s12/ex/Exercise.java
+++ src/main/java/m09/s12/ex/Exercise.java
@@ -1,31 +1,35 @@
 package m09.s12.ex;
 
 /**
- * TODO:
- *
- * Implement MyIntStack, an int stack as a list of nodes, supporting pop() and
- * push()
- */
-/**
- * TODO:
- *
- * Implement an int stack, MyIntStack, as a list of Node (nested class)
+ * TODO: Implement an int stack, MyIntStack, based on Node (nested class)
  * 
- * minimal set of methods: push(), pop() and toString()
+ * <ul>
+ * Each Node should keep
+ * <li>the current value (an int)
+ * <li>the reference to the previous element in the stack (if any)
+ * </ul>
  * 
- * let it throw an unchecked exception in case of errors
+ * minimal set of required methods (unchecked exception in case of error):
+ * <li>push()
+ * <li>pop()
+ * <li>toString()
  */
 public class Exercise {
     public static void main(String[] args) {
         MyIntStack mis = new MyIntStack();
 
+        System.out.println("The empty stack: " + mis.toString());
+
         for (int i = 0; i < 5; i++) {
             mis.push(i);
         }
 
+        System.out.println("The stack with five elements in it: " + mis.toString());
+
         for (int i = 0; i < 5; i++) {
             try {
-                System.out.println(mis.pop());
+                System.out.println("Popping an element: " + mis.pop());
+                System.out.println("Now the stack is: " + mis.toString());
             } catch (Exception e) {
                 e.printStackTrace();
             }
