4cf6b027b8e152c40fa858b26b81e1e353ec85c1
Emanuele Galli <egalli64@gmail.com>
2021-02-07 21:18:46 +0100

    mapToInt example

--- src/main/java/m11/s12/PrimitiveStreamExample.java
+++ src/main/java/m11/s12/PrimitiveStreamExample.java
@@ -1,6 +1,7 @@
 package m11.s12;
 
 import java.util.Arrays;
+import java.util.OptionalInt;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -13,15 +14,21 @@ public class PrimitiveStreamExample {
         Arrays.stream(data).reduce(Integer::sum) //
                 .ifPresent(x -> System.out.println("No boxing/unboxing to calculate sum: " + x));
 
-        // boxing a stream
+        // boxing a stream: da IntStream a Stream<Integer>
         Stream<Integer> si = Arrays.stream(data).boxed();
         si.reduce(Integer::sum) //
                 .ifPresent(x -> System.out.println("No boxing/unboxing to calculate sum: " + x));
 
+        // IntStream, IntPredicate, IntConsumer
         System.out.println("Even numbers in [0, 10) ...");
         IntStream.range(0, 10).filter(x -> x % 2 == 0).forEach(System.out::println);
 
         System.out.println("Even numbers in [0, 10] ...");
         IntStream.rangeClosed(0, 10).filter(x -> x % 2 == 0).forEach(System.out::println);
+
+        // Stream<String>, MapToInt, IntStream, OptionalInt, IntConsumer
+        OptionalInt result = Stream.of("Tom", "Robert", "Jonathan").mapToInt(String::length).max();
+        System.out.print("The longest string length is ");
+        result.ifPresentOrElse(System.out::println, () -> System.out.println("unknown!"));
     }
 }
