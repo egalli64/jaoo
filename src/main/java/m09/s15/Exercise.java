0dc4e99e732f21c9bf120f55253bb6b9aedc444f
Emanuele Galli <egalli64@gmail.com>
2021-02-08 14:38:49 +0100

    more info

--- src/main/java/m09/s15/Exercise.java
+++ src/main/java/m09/s15/Exercise.java
@@ -4,6 +4,20 @@ import java.util.List;
 
 public class Exercise {
     /**
+     * Extract from input the even values and return them in an array
+     * 
+     * Do not use Collections in here!
+     * 
+     * @param data a bunch of integers
+     * @return even values from input
+     * 
+     *         example: in {2, 3, 4, 5} -> out {2, 4}
+     */
+    public int[] evensRaw(int[] data) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
      * Extract from input the even values and return them in a List
      * 
      * @param data a bunch of integers
@@ -15,17 +29,13 @@ public class Exercise {
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    public int[] evensRaw(int[] data) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
     /**
      * Extract non-duplicated values in input and return them in a list
      * 
      * @param data a bunch of integers
      * @return only the non-duplicated values from input
      * 
-     *         example: in {2, 3, 4, 4, 5, 5, 6, 7} -> out {2, 3}
+     *         example: in {2, 3, 4, 4, 5, 5, 6, 7} -> out {2, 3, 6, 7}
      */
     public List<Integer> singles(int[] data) {
         throw new UnsupportedOperationException("Not yet implemented");
