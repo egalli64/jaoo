f90202c932c433a99dd12c45c9543bf3a12e7039
Emanuele Galli <egalli64@gmail.com>
2021-02-18 08:21:05 +0100

    Synchro examples

---
+++ src/main/java/m10/s05/SynchronizedHello.java
@@ -0,0 +1,14 @@
+package m10.s05;
+
+public class SynchronizedHello {
+    synchronized public void sayHello(String name) {
+        System.out.print("Hello ");
+        try {
+            // job simulation
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            System.out.println("Interrupted");
+        }
+        System.out.println(name + "!");
+    }
+}
