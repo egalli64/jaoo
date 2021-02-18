32d780184c5a9391eebbdae8b1d7337389aa70ac
Emanuele Galli <egalli64@gmail.com>
2021-02-18 08:51:37 +0100

    Mutex example

---
+++ src/main/java/m10/s05/WMMode.java
@@ -0,0 +1,5 @@
+package m10.s05;
+
+public enum WMMode {
+    ON_THIS, ON_MUTEX_F, ON_MUTEX_G
+}
---
+++ src/main/java/m10/s05/WithMutexExample.java
@@ -0,0 +1,74 @@
+package m10.s05;
+
+public class WithMutexExample {
+    private Object mutexF = new Object();
+    private Object mutexG = new Object();
+
+    public static void main(String[] args) {
+        WithMutexExample wme = new WithMutexExample();
+
+        Thread[] threads = { //
+                new Thread(new WMRunner(wme, WMMode.ON_THIS)), //
+                new Thread(new WMRunner(wme, WMMode.ON_MUTEX_F)), //
+                new Thread(new WMRunner(wme, WMMode.ON_MUTEX_G)), //
+                new Thread(new WMRunner(wme, WMMode.ON_THIS)), //
+                new Thread(new WMRunner(wme, WMMode.ON_MUTEX_F)), //
+                new Thread(new WMRunner(wme, WMMode.ON_MUTEX_G)) //
+        };
+
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                System.out.println("Join interrupted on " + t.getName());
+            }
+        }
+        System.out.println("Main thread done");
+    }
+
+    public synchronized void syncOnThis() {
+        System.out.println("Enter syncOnThis() for " + Thread.currentThread().getName());
+
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            System.out.println("Sleep interrupted on syncOnThis()");
+        }
+
+        System.out.println("Exit syncOnThis() for " + Thread.currentThread().getName());
+    }
+
+    public void syncOnMutexF() {
+
+        synchronized (mutexF) {
+            System.out.println("Enter monitor in syncOnMutexF() for " + Thread.currentThread().getName());
+
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                System.out.println("Sleep interrupted on syncOnMutexF()");
+            }
+
+            System.out.println("Exit monitor in syncOnMutexF() for " + Thread.currentThread().getName());
+        }
+    }
+
+    public void syncOnMutexG() {
+
+        synchronized (mutexG) {
+            System.out.println("Enter monitor in syncOnMutexG() for " + Thread.currentThread().getName());
+
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                System.out.println("Sleep interrupted on syncOnMutexG()");
+            }
+
+            System.out.println("Exit monitor in syncOnMutexG() for " + Thread.currentThread().getName());
+        }
+    }
+}
