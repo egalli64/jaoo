f150c8b623563047530fd2e9c22e41fedd5ac542
Emanuele Galli <egalli64@gmail.com>
2021-02-19 08:38:50 +0100

    of / copyOf examples

--- src/main/java/m09/s09/AsSortedSet.java
+++ src/main/java/m09/s09/AsSortedSet.java
@@ -1,22 +1,24 @@
 package m09.s09;
 
-import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class AsSortedSet {
     public static void main(String[] args) {
-        Collection<Integer> coll = List.of(12, 18, -5, -2233);
-        SortedSet<Integer> sortedSet = new TreeSet<Integer>(coll);
-        System.out.println(coll + " -> " + sortedSet);
+        Set<Integer> set = Set.of(12, 18, -5, -2233);
+        System.out.println("Set: " + set);
+
+        List<Integer> list = List.of(12, 18, -5, -2233, 12, 18);
+        System.out.println("List: " + list);
+        System.out.println("From list to set: " + Set.copyOf(list));
+
+        SortedSet<Integer> sortedSet = new TreeSet<Integer>(set);
+        System.out.println(set + " sorted -> " + sortedSet);
         System.out.println("First: " + sortedSet.first());
         System.out.println("Last: " + sortedSet.last());
+
         System.out.println("Subset [second, last): " + sortedSet.subSet(sortedSet.first() + 1, sortedSet.last()));
     }
 }
-
-// [-2233, -5, 12, 18]
-// . . . -5 . . .
-// -2233 ... 18
-//  ... ... 12 20
