0c23fa9665229ba0a3fd5a8c79dbc6a1644d3cac
Emanuele Galli <egalli64@gmail.com>
2021-04-27 08:24:48 +0200

    simplify

--- src/main/java/m01/s06/bugs/Overflow.java
+++ src/main/java/m01/s06/bugs/Overflow.java
@@ -2,12 +2,12 @@ package m01.s06.bugs;
 
 public class Overflow {
     public static void main(String[] args) {
-        int[] accounts = { 1_000_000_001, 1_000_000_002, 1_000_000_003, 1_000_000_004 };
+        int bob = 1_000_000_001;
+        int tom = 1_000_000_002;
+        int kim = 1_000_000_003;
+        int jon = 1_000_000_004;
 
-        int total = accounts[0];
-        total = total + accounts[1];
-        total = total + accounts[2];
-        total = total + accounts[3];
+        int total = bob + tom + kim + jon;
 
         System.out.println(total);
     }
