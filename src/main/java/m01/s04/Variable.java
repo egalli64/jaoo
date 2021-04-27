84d4b09afa127b14020d1b7c6fe1fea93b844114
Emanuele Galli <egalli64@gmail.com>
2021-04-27 22:26:33 +0200

    more comments

--- src/main/java/m01/s04/Variable.java
+++ src/main/java/m01/s04/Variable.java
@@ -2,25 +2,28 @@ package m01.s04;
 
 public class Variable {
     public static void main(String[] args) {
-        int aPrimitive = 42;
-        String aReference = "42";
+        int aPrimitiveVariable = 42;
+        String aReferenceVariable = "42";
 
-        System.out.println(aReference);
-        System.out.println(aPrimitive);
+        System.out.println(aReferenceVariable);
+        System.out.println(aPrimitiveVariable);
 
         // Dangerous! Beware of null!
         String noReference = null;
         System.out.println(noReference);
 
         // Java 10 inferred type
-        var anotherPrimitive = 12;
-        var anotherReference = "Tom";
+        var anotherPrimitiveVariable = 12;
+        var anotherReferenceVariable = "Tom";
 
-        System.out.println(anotherReference);
-        System.out.println(anotherPrimitive);
+        System.out.println(anotherReferenceVariable);
+        System.out.println(anotherPrimitiveVariable);
 
         final int A_CONSTANT = 12;
         final var ANOTHER_CONSTANT = 21;
         System.out.println(A_CONSTANT + ANOTHER_CONSTANT);
+
+        // a final variable can't change!
+//        A_CONSTANT = 42;
     }
 }
