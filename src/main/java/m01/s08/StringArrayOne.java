dd1b6a81478d2291fc40e3832bf720ba52f23447
Emanuele Galli <egalli64@gmail.com>
2021-04-28 08:58:44 +0200

    more examples

---
+++ src/main/java/m01/s08/StringArrayOne.java
@@ -0,0 +1,12 @@
+package m01.s08;
+
+import java.util.Arrays;
+
+public class StringArrayOne {
+    public static void main(String[] args) {
+        String[] one = { "ciao", "buongiorno", "hello", "good morning" };
+
+        System.out.println(one);
+        System.out.println(Arrays.toString(one));
+    }
+}
