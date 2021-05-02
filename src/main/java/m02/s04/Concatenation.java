6bb6f8d9813f7451d257e929f72e08ad57a3105c
Emanuele Galli <egalli64@gmail.com>
2021-05-02 20:29:48 +0200

    more comments

--- src/main/java/m02/s04/Concatenation.java
+++ src/main/java/m02/s04/Concatenation.java
@@ -2,13 +2,17 @@ package m02.s04;
 
 public class Concatenation {
     public static void main(String[] args) {
+        System.out.print("5 + 7: ");
         System.out.println(5 + 7);
+
+        System.out.print("5 + \"7\": ");
         System.out.println(5 + "7");
 
         System.out.println("Resistence" + " is " + "useless");
         System.out.println("Solution: " + 42);
         System.out.println(true + " or " + false);
 
+        System.out.print("Repeat \"Vogons\" three times: ");
         System.out.println("Vogons".repeat(3)); // Java 11
 
         String s = "Hello";
@@ -18,7 +22,10 @@ public class Concatenation {
         // reference to a different object created by concatenation
         s += 3;
 
+        System.out.print("+= on a string generate a new object: ");
         System.out.println(s);
+
+        System.out.print("We could still have access to the original string: ");
         System.out.println(t);
     }
 }
