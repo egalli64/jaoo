1a66929bf6c49f605482be3c15a2e9a1c0e5435b
Emanuele Galli <egalli64@gmail.com>
2021-05-02 19:10:28 +0200

    more examples

--- src/main/java/m02/s03/Arithmetic.java
+++ src/main/java/m02/s03/Arithmetic.java
@@ -5,38 +5,83 @@ public class Arithmetic {
         int a = 10;
         int b = 3;
 
+        System.out.print("10 + 3 = ");
         System.out.println(a + b); // 13
+
+        System.out.print("10 - 3 = ");
         System.out.println(a - b); // 7
+
+        System.out.print("10 * 3 = ");
         System.out.println(a * b); // 30
+
+        System.out.print("10 / 3 = ");
         System.out.println(a / b); // 3
+
+        System.out.print("10 % 3 = ");
         System.out.println(a % b); // 1
 
+        System.out.print("Overflow! 1_000_000 * 3_000 = ");
+        System.out.println(1_000_000 * 3_000); // 30
+
+        System.out.println("---");
 //        System.out.println(a / 0); // ArithmeticException
 
         double c = 3.0;
 
+        System.out.print("10 + 3.0 = ");
         System.out.println(a + c); // 13.0
+
+        System.out.print("10 / 3.0 = ");
+
+        System.out.print("10 / 3.0 = ");
         System.out.println(a / c); // 3.3333333333333335
+
+        System.out.print("10 % 3.0 = ");
         System.out.println(a % c); // 1.0
 
+        System.out.print("3.0 - 2.1 = ");
         System.out.println(c - 2.1); // 0.8999999999999999
+
+        System.out.print("3.0 / 0 = ");
         System.out.println(c / 0); // Infinity
 
+        System.out.println("--- alpha = 2 ---");
         int alpha = 2;
 
         alpha += 8; // 10
+        System.out.print("alpha += 8 --> alpha = ");
         System.out.println(alpha);
 
         alpha -= 3; // 7
+        System.out.print("alpha -= 3 --> alpha = ");
         System.out.println(alpha);
 
         alpha *= 2; // 14
+        System.out.print("alpha *= 2 --> alpha = ");
         System.out.println(alpha);
 
         alpha /= 2; // 7
+        System.out.print("alpha /= 2 --> alpha = ");
         System.out.println(alpha);
 
         alpha %= 5; // 2
+        System.out.print("alpha %= 5 --> alpha = ");
         System.out.println(alpha);
+
+        alpha += 3_000_000_000L;
+        System.out.print("alpha += 3_000_000_000L --> alpha = ");
+        System.out.println(alpha);
+
+        System.out.println("--- adding two bytes you get an int! ---");
+        byte aByte = 6;
+        byte anotherByte = 42;
+        byte resultingByte = (byte) (aByte + anotherByte);
+        System.out.println(resultingByte);
+
+        System.out.println("--- ... unless their are constants ---");
+        final byte fb1 = 6;
+        final byte fb2 = 42;
+        resultingByte = fb1 + fb2;
+        System.out.println(resultingByte);
     }
 }
