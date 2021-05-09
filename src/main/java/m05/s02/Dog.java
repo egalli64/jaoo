8e1f3a31182807c824ba4925d3371119c6d2d235
Emanuele Galli <egalli64@gmail.com>
2021-05-09 23:41:19 +0200

    more details

--- src/main/java/m05/s02/Dog.java
+++ src/main/java/m05/s02/Dog.java
@@ -2,5 +2,6 @@ package m05.s02;
 
 public class Dog {
     void bark() {
+        System.out.println("Woof");
     }
 }
--- src/main/java/m05/s02/Main.java
+++ src/main/java/m05/s02/Main.java
@@ -1,8 +1,18 @@
 package m05.s02;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.bark();
+        // no dog created here
+        Dog[] dogs = new Dog[2];
+        System.out.println(Arrays.toString(dogs));
+
+        Dog tom = new Dog();
+        dogs[0] = tom;
+        dogs[1] = new Dog();
+        dogs[0].bark();
+        dogs[1].bark();
+        tom.bark();
     }
 }
