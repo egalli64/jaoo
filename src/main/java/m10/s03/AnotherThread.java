90448fc7888520d727e1fa3d055ce5887ab50f7e
Emanuele Galli <egalli64@gmail.com>
2021-06-28 21:12:36 +0200

    sleep example

--- src/main/java/m10/s03/AnotherThread.java
+++ src/main/java/m10/s03/AnotherThread.java
@@ -3,6 +3,16 @@ package m10.s03;
 public class AnotherThread extends Thread {
     @Override
     public void run() {
-        System.out.println("A message from AnotherThread");
+        System.out.println("A message from an AnotherThread on thread " + Thread.currentThread().getName());
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(String.format("Message %d from %s", i, Thread.currentThread().getName()));
+
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + " interrupted");
+            }
+        }
     }
 }
--- src/main/java/m10/s03/RunnableClass.java
+++ src/main/java/m10/s03/RunnableClass.java
@@ -9,7 +9,17 @@ public class RunnableClass implements Runnable {
 
     @Override
     public void run() {
-        System.out.println("A message from a Runnable Class");
+        System.out.println("A message from a RunnableClass on thread " + Thread.currentThread().getName());
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(String.format("%s message %d", Thread.currentThread().getName(), i));
+
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + " interrupted");
+            }
+        }
     }
 
     public void start() {
