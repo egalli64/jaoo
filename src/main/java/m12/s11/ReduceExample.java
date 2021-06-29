f74e951d55cfc546601050f683df562035fad791
Emanuele Galli <egalli64@gmail.com>
2021-06-29 07:44:17 +0200

    more comments

--- src/main/java/m12/s11/ReduceExample.java
+++ src/main/java/m12/s11/ReduceExample.java
@@ -8,23 +8,23 @@ public class ReduceExample {
         System.out.println(data);
 
         Integer sum = data.stream().reduce(0, (x, y) -> x + y);
-        System.out.println("The sum is " + sum);
+        System.out.println("Adding up elements explicitly: " + sum);
 
         data.stream().reduce(Integer::sum) //
-                .ifPresent(z -> System.out.println("The sum is " + z));
+                .ifPresent(z -> System.out.println("Adding up elements by sum: " + z));
 
         data.stream().reduce(Integer::max) //
-                .ifPresent(z -> System.out.println("The largest value is " + z));
+                .ifPresent(z -> System.out.println("Largest value by max: " + z));
 
         data.stream().reduce(Integer::min) //
-                .ifPresent(z -> System.out.println("The smallest value is " + z));
+                .ifPresent(z -> System.out.println("Smallest value by min: " + z));
 
         // map-reduce example
         data.stream().map(x -> x % 2 == 0 ? 0 : 1).reduce(Integer::sum) //
-                .ifPresent(z -> System.out.println("Count of odd numbers: " + z));
+                .ifPresent(z -> System.out.println("Count of odd numbers by map-reduce: " + z));
 
-        // in this case, using map-reduce is an overkill
-        System.out.println("Count of odd numbers: " + //
+        // in this last case, using map-reduce is an overkill
+        System.out.println("Count of odd numbers by count: " + //
                 data.stream().filter(x -> x % 2 == 1).count());
     }
 }
