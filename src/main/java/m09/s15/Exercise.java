efa42966403ba93c86dd2ea13b32d1fbc67fffc7
Emanuele Galli <egalli64@gmail.com>
2021-08-07 18:22:52 +0200

    cleanup

--- src/main/java/m09/s15/Exercise.java
+++ src/main/java/m09/s15/Exercise.java
@@ -1,6 +1,5 @@
 package m09.s15;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Exercise {
@@ -13,6 +12,10 @@ public class Exercise {
      * @return even values from input
      * 
      *         example: in {2, 3, 4, 5} -> out {2, 4}
+     * 
+     *         example: in {2, 2, 4, 6} -> out {2, 2, 4, 6}
+     * 
+     *         example: in {2, 3, 4, 6} -> out {2, 4, 6}
      */
     public int[] evensRaw(int[] data) {
         throw new UnsupportedOperationException("Not yet implemented");
@@ -27,15 +30,7 @@ public class Exercise {
      *         example: in {2, 3, 4, 5} -> out {2, 4}
      */
     public List<Integer> evens(int[] data) {
-        List<Integer> result = new ArrayList<>();
-
-        for (int value : data) {
-            if (value % 2 == 0) {
-                result.add(value);
-            }
-        }
-
-        return result;
+        throw new UnsupportedOperationException("Not yet implemented");
     }
 
     /**
@@ -48,20 +43,5 @@ public class Exercise {
      */
     public List<Integer> singles(int[] data) {
         throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    /**
-     * As above, but with a linear time algorithm
-     * 
-     * @param data
-     * @return
-     */
-    public List<Integer> singlesLinear(int[] data) {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    public static void main(String[] args) {
-        Exercise ex = new Exercise();
-        System.out.println(ex.singlesLinear(new int[] { 2, 3, 4, 4, 5, 6 }));
     }
 }
