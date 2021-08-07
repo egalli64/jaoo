83ea44e334a2e1b9100001c2c1f2ce1bcdbbb1db
Emanuele Galli <egalli64@gmail.com>
2021-08-07 13:02:47 +0200

    more comments

--- src/main/java/m11/s13/Main.java
+++ src/main/java/m11/s13/Main.java
@@ -1,6 +1,7 @@
 package m11.s13;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -8,9 +9,11 @@ public class Main {
         List<Integer> data = Arrays.asList(42, 7, 5, 12);
         System.out.println("input: " + data);
 
+        // using external comparator
         data.sort(new OddFirst());
         System.out.println("ordered, odd first: " + data);
 
+        // comparator as lambda
         data.sort((left, right) -> {
             if (left % 2 == 0 && right % 2 == 1) {
                 return -1;
@@ -21,5 +24,11 @@ public class Main {
             return left.compareTo(right);
         });
         System.out.println("ordered, even first: " + data);
+
+        // (ab)using ternary operator to squeeze the code
+        Comparator<Integer> c = (left, right) -> left % 2 == 1 && right % 2 == 0 ? -1 : //
+                left % 2 == 0 && right % 2 == 1 ? 1 : left.compareTo(right);
+        data.sort(c);
+        System.out.println("ordered, odd first: " + data);
     }
 }
