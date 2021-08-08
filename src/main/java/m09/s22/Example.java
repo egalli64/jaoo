7417e32407ef88ede7510b454b504b8e37f5177d
Emanuele Galli <egalli64@gmail.com>
2021-08-08 21:38:37 +0200

    minor cleanup

--- src/main/java/m09/s22/Example.java
+++ src/main/java/m09/s22/Example.java
@@ -1,25 +1,20 @@
 package m09.s22;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Example {
     public static void main(String[] args) {
         // name -> phone number
-        // "Tom" -> "0044 3242344234234"
-        // "Bill" -> "3242344234234"
-
-        // ? Tom -> "0044 3242344234234"
-        // ? Bob -> "no number!"
-        Map<String, String> map = new TreeMap<>();
+        Map<String, String> map = new HashMap<>();
         map.put("Tom", "43534534534534");
         map.put("Bob", "78768679789");
 
-        for (Map.Entry<String, String> entry : map.entrySet()) {
+        for (var entry : map.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
         }
 
-        for (var key : map.keySet()) {
+        for (String key : map.keySet()) {
             System.out.println(key + ": " + map.get(key));
         }
     }
--- src/main/java/m09/s22/Exercise.java
+++ src/main/java/m09/s22/Exercise.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 public class Exercise {
     /**
-     * Extract non-duplicated values in input and return them in a list
+     * Extract non-duplicated values in input and return them in a list, for example:
+     * <li>in {2, 3, 4, 4, 4, 5, 5, 5, 6, 7} -> out {2, 3, 6, 7}
+     * <li>in {4, 5, 4, 2, 5, 3, 6, 7} -> out {2, 3, 6, 7}
      * 
      * @param data a bunch of integers
      * @return only the non-duplicated values from input
-     * 
-     *         example: in {2, 3, 4, 4, 5, 5, 6, 7} -> out {2, 3, 6, 7}
      */
     public List<Integer> singles(int[] data) {
         // TODO
@@ -20,5 +20,4 @@ public class Exercise {
         List<Integer> result = new Exercise().singles(new int[] { 2, 3, 4, 4, 5, 5, 6, 7 });
         System.out.println(result);
     }
-
 }
