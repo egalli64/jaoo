fb881cc741eb50418be07a89a85841024b0dddf2
Emanuele Galli <egalli64@gmail.com>
2021-08-19 22:18:36 +0200

    read() vs readLine()

--- src/main/java/m08/s10/ReadHello.java
+++ src/main/java/m08/s10/ReadHello.java
@@ -4,10 +4,15 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ReadHello {
+    private static final Logger log = Logger.getGlobal();
+    private static final String FILE_NAME = "/tmp/hello.txt";
+
     public static void main(String[] args) {
-        File f = new File("/tmp/hello.txt");
+        File f = new File(FILE_NAME);
 
         BufferedReader br = null;
         try {
@@ -15,13 +20,28 @@ public class ReadHello {
             String line = br.readLine();
             System.out.println(">" + line + "<");
 
+            System.out.println("***");
+            int cur;
+            while ((cur = br.read()) != -1) {
+                char c = (char) cur;
+                if (c == '\n') {
+                    System.out.println();
+                } else if (c != '\r') {
+                    System.out.print("_");
+                    System.out.print(c);
+                    System.out.print("_");
+                }
+            }
+            System.out.println();
+
+            log.info("Done reading from " + FILE_NAME);
         } catch (IOException e) {
-            e.printStackTrace();
+            log.log(Level.SEVERE, "Can't read from " + FILE_NAME, e);
         } finally {
             try {
                 br.close();
-            } catch (IOException | NullPointerException e) {
-                e.printStackTrace();
+            } catch (IOException e) {
+                log.log(Level.SEVERE, "Can't close on " + FILE_NAME, e);
             }
         }
     }
