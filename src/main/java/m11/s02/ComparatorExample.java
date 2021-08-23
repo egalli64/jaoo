1f3d1e0c067302bd1e247936594c2690c98a9b62
Emanuele Galli <egalli64@gmail.com>
2021-08-23 14:58:01 +0200

    comparing and reversed example

--- src/main/java/m11/s02/ComparatorExample.java
+++ src/main/java/m11/s02/ComparatorExample.java
@@ -1,6 +1,7 @@
 package m11.s02;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class ComparatorExample {
     public static void main(String[] args) {
@@ -9,5 +10,9 @@ public class ComparatorExample {
 
         Arrays.sort(names, (String left, String right) -> left.length() - right.length());
         System.out.println("From shorter to longer: " + Arrays.toString(names));
+
+        // a few methods are available to make the job even simpler
+        Arrays.sort(names, Comparator.comparingInt(String::length).reversed());
+        System.out.println("From longer to shorter: " + Arrays.toString(names));
     }
 }
