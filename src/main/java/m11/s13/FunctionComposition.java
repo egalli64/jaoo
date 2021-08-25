899a2e1f1c9e9d45e79b122045caf6d73c8dfef2
Emanuele Galli <egalli64@gmail.com>
2021-08-25 22:03:40 +0200

    composition

---
+++ src/main/java/m11/s13/FunctionComposition.java
@@ -0,0 +1,17 @@
+package m11.s13;
+
+import java.util.function.Function;
+import java.util.function.IntFunction;
+
+public class FunctionComposition {
+    public static void main(String[] args) {
+        Function<Integer, Integer> doubler = x -> x * 2;
+        Function<Integer, Integer> squarer = x -> x * x;
+
+        Function<Integer, Integer> doubledAndSquarer = doubler.andThen(squarer);
+        System.out.println("3 doubled and then squared: " + doubledAndSquarer.apply(3));
+
+        Function<Integer, Integer> doubledComposedSquare = doubler.compose(squarer);
+        System.out.println("3 composed by doubler and then squarer: " + doubledComposedSquare.apply(3));
+    }
+}
---
+++ src/main/java/m11/s13/PredicateComposition.java
@@ -0,0 +1,28 @@
+package m11.s13;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class PredicateComposition {
+    public static void main(String[] args) {
+        List<Dog> dogs = Arrays.asList(new Dog("Bob", "Robert Mitchum", 11),
+                new Dog("Rob", "Robert Redford", 4),
+                new Dog("Bob", "Robert Redford", 11)
+        );
+
+        System.out.println("A dog list: " + dogs);
+
+        Predicate<Dog> old = dog -> dog.getAge() > 10;
+        System.out.println("Old dogs: " + dogs.stream().filter(old).collect(Collectors.toList()));
+        System.out.println("Non-old dogs: " + dogs.stream().filter(old.negate()).collect(Collectors.toList()));
+
+        Predicate<Dog> redfordOwned = dog -> dog.getOwner().endsWith("Redford");
+        System.out.println("Old dogs owned by Redford: " + dogs.stream().filter(old.and(redfordOwned)).collect(Collectors.toList()));
+
+        Predicate<Dog> namedRob = dog -> dog.getName().equals("Rob");
+        System.out.println("Not (named Rob or owned by Redford): " + dogs.stream().filter(namedRob.or(redfordOwned).negate()).collect(Collectors.toList()));
+
+    }
+}
