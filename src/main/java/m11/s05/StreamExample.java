f98b940b014555efd4a267a10982f395a7f50aab
Emanuele Galli <egalli64@gmail.com>
2021-08-31 18:35:29 +0200

    code cleanup

--- src/main/java/m11/s05/StreamExample.java
+++ src/main/java/m11/s05/StreamExample.java
@@ -2,24 +2,22 @@ package m11.s05;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class StreamExample {
     public static void main(String[] args) {
-        List<Integer> values = List.of(12, 34, 2, 11, 5, 7);
-        System.out.println("Starting from this collection:" + values);
+        List<Integer> data = List.of(12, 34, 2, 11, 5, 7);
+        System.out.println("Starting from this collection:" + data);
 
         // without stream
-        List<Integer> evenValues = extractEvenValuesNoStream(values);
         System.out.println("Even numbers (no stream)");
-        evenValues.forEach(System.out::println);
+        extractEvenValues(data).forEach(System.out::println);
 
         // with stream
         System.out.println("Even numbers (stream)");
-        values.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
+        data.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
     }
 
-    private static List<Integer> extractEvenValuesNoStream(List<Integer> values) {
+    private static List<Integer> extractEvenValues(List<Integer> values) {
         List<Integer> result = new ArrayList<>();
 
         // consumes the input list, adding its even elements to the result
