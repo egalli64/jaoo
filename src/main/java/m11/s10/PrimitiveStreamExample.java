a95caf600eac0486217d9d1f7085610f56db40b9
Emanuele Galli <egalli64@gmail.com>
2021-08-31 19:10:19 +0200

    code cleanup

--- src/main/java/m11/s10/PrimitiveStreamExample.java
+++ src/main/java/m11/s10/PrimitiveStreamExample.java
@@ -1,23 +1,20 @@
 package m11.s10;
 
 import java.util.Arrays;
-import java.util.OptionalInt;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class PrimitiveStreamExample {
     public static void main(String[] args) {
-        int[] data = { 4, 2, 5, 23, 54, 11, 67, 100, 15, 83, 45 };
+        int[] data = {4, 2, 5, 23, 54, 11, 67, 100, 15, 83, 45};
         System.out.println(Arrays.toString(data));
 
         // IntStream, IntBinaryOperator, OptionalInt, IntConsumer
         Arrays.stream(data).reduce(Integer::sum) //
-                .ifPresent(x -> System.out.println("No boxing/unboxing to calculate sum: " + x));
+                .ifPresent(x -> System.out.println("All-primitive sum: " + x));
 
-        // boxing a stream: da IntStream a Stream<Integer>
-        Stream<Integer> si = Arrays.stream(data).boxed();
-        si.reduce(Integer::sum) //
-                .ifPresent(x -> System.out.println("No boxing/unboxing to calculate sum: " + x));
+        // boxing a stream: from IntStream to Stream<Integer>, then "normal" Optional and Consumer are used
+        Arrays.stream(data).boxed().reduce(Integer::sum) //
+                .ifPresent(x -> System.out.println("Working on boxed integers: " + x));
 
         // IntStream, IntPredicate, IntConsumer
         System.out.println("Even numbers in [0, 10) ...");
@@ -26,9 +23,12 @@ public class PrimitiveStreamExample {
         System.out.println("Even numbers in [0, 10] ...");
         IntStream.rangeClosed(0, 10).filter(x -> x % 2 == 0).forEach(System.out::println);
 
-        // Stream<String>, MapToInt, IntStream, OptionalInt, IntConsumer
-        OptionalInt result = Stream.of("Tom", "Robert", "Jonathan").mapToInt(String::length).max();
-        System.out.print("The longest string length is ");
-        result.ifPresentOrElse(System.out::println, () -> System.out.println("unknown!"));
+        String[] names = {"Tom", "Robert", "Jonathan"};
+        System.out.println("Names: " + Arrays.toString(names));
+
+        // Stream<String>, ToIntFunction, IntStream, OptionalInt, IntConsumer
+        System.out.print("The longest name has length ");
+        Arrays.stream(names).mapToInt(String::length).max()
+                .ifPresentOrElse(System.out::println, () -> System.out.println("unknown!"));
     }
 }
