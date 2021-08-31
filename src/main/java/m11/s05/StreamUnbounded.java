30a71ecb583fd354797fd9b9c197e01550089ac6
Emanuele Galli <egalli64@gmail.com>
2021-08-31 09:36:55 +0200

    simplify

--- src/main/java/m11/s05/StreamUnbounded.java
+++ src/main/java/m11/s05/StreamUnbounded.java
@@ -5,13 +5,13 @@ import java.util.stream.Stream;
 
 public class StreamUnbounded {
     public static void main(String[] args) {
-        var evenNumbers = Stream.iterate(2, x -> x + 2).limit(5).collect(toList());
-        System.out.println("The first five even numbers: " + evenNumbers);
+        System.out.println("The first five even numbers");
+        Stream.iterate(2, x -> x + 2).limit(5).forEach(System.out::println);
 
-        var oddNumbers = Stream.iterate(1, x -> x < 10, x -> x + 2).collect(toList());
-        System.out.println("Odd numbers less than 10: " + oddNumbers);
+        System.out.println("Odd numbers less than 10");
+        Stream.iterate(1, x -> x < 10, x -> x + 2).forEach(System.out::println);
 
-        var randomNumbers = Stream.generate(Math::random).limit(5).collect(toList());
-        System.out.println("Five random numbers in [0, 1): " + randomNumbers);
+        System.out.println("Five random numbers in [0, 1)");
+        Stream.generate(Math::random).limit(5).forEach(System.out::println);
     }
 }
