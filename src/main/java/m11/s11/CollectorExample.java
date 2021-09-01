09679fbe0304bf3270e39d7e033d4901cbb8c5e1
Emanuele Galli <egalli64@gmail.com>
2021-09-01 09:06:32 +0200

    simplify

--- src/main/java/m11/s11/CollectorExample.java
+++ src/main/java/m11/s11/CollectorExample.java
@@ -1,29 +1,20 @@
 package m11.s11;
 
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.toList;
-
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CollectorExample {
     public static void main(String[] args) {
-        List<Integer> data = List.of(4, 2, 5, -23, 54, -11, 67, -100, 15, -83, 45);
+        List<Integer> values = List.of(4, 2, 5, -23, 54, -11, 67, -100, 15, -83, 45);
+        System.out.println("Values: " + values);
 
-        System.out.println("Values: " + data.stream().collect(toList()));
+        // toList()
+        System.out.println("Positive values: " +
+                values.stream().filter(x -> x > 0).collect(Collectors.toList()));
 
-        // counting
-        System.out.println("Counting: " + data.stream().collect(counting()));
-        System.out.println("Count: " + data.stream().count());
-
-        // max & min
-        data.stream().collect(Collectors.maxBy(Comparator.naturalOrder())) //
-                .ifPresent(x -> System.out.println("Max by: " + x));
-        data.stream().collect(Collectors.minBy(Comparator.naturalOrder())) //
-                .ifPresent(x -> System.out.println("Min by: " + x));
-        data.stream().max(Comparator.naturalOrder()).ifPresent(x -> System.out.println("Max: " + x));
-        data.stream().min(Comparator.naturalOrder()).ifPresent(x -> System.out.println("Min: " + x));
-
+        // counting()
+        System.out.println("Counting positive values: " +
+                values.stream().filter(x -> x > 0).collect(Collectors.counting()));
+        System.out.println("Same, using count(): " + values.stream().filter(x -> x > 0).count());
     }
 }
