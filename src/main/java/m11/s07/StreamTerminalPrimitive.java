1b59f50a7d583e1716372e18cfd8bd053614d5ee
Emanuele Galli <egalli64@gmail.com>
2021-09-01 11:31:32 +0200

    moved slide

---
+++ src/main/java/m11/s07/StreamTerminalPrimitive.java
@@ -0,0 +1,16 @@
+package m11.s07;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamTerminalPrimitive {
+    public static void main(String[] args) {
+        int[] values = {11, 34, 2, 11, 5, 2};
+        System.out.println("Original data: " + Arrays.toString(values));
+
+        System.out.println("Even values");
+        Arrays.stream(values).filter(x -> x % 2 == 0).forEach(System.out::println);
+
+        System.out.println("Count even values: " + Arrays.stream(values).filter(x -> x % 2 == 0).count());
+    }
+}
