75a48bc6d0d81ae77d3aa2ac2c1054a0df090504
Emanuele Galli <egalli64@gmail.com>
2021-09-01 11:17:51 +0200

    simplify

--- src/main/java/m11/s06/PrimitiveStreamExample.java
+++ src/main/java/m11/s06/PrimitiveStreamExample.java
@@ -5,18 +5,15 @@ import java.util.stream.IntStream;
 
 public class PrimitiveStreamExample {
     public static void main(String[] args) {
-        int[] data = {4, 2, 5, 23, 54, 11, 67, 101, 15, 83, 45};
+        int[] data = {4, 2, 5, 23, 154, 11, 67, 101, 15, 83, 45};
         System.out.println(Arrays.toString(data));
 
-        System.out.println("Looking for a value above 100");
-        // IntStream, IntPredicate, OptionalInt, IntConsumer
-        Arrays.stream(data).filter(x -> x > 100).findAny() //
-                .ifPresentOrElse(x -> System.out.println("Found " + x), () -> System.out.println("Nothing found"));
+        System.out.println("Values above 100");
+        // IntStream, IntPredicate, IntConsumer
+        Arrays.stream(data).filter(x -> x > 100).forEach(System.out::println);
 
-
-        // boxing a stream: from IntStream to Stream<Integer>, then "normal" Optional, Predicate and Consumer are used
-        Arrays.stream(data).boxed().filter(x -> x > 100).findAny() //
-                .ifPresent(x -> System.out.println("Working on boxed integers: " + x));
+        // boxing a stream: from IntStream to Stream<Integer>, then "normal" Predicate and Consumer are used
+        Arrays.stream(data).boxed().filter(x -> x > 100).forEach(System.out::println);
 
         // IntStream, IntPredicate, IntConsumer
         System.out.println("Even numbers in [0, 10) ...");
