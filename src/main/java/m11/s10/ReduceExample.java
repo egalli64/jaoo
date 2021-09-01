a5cf1a29a7472fc694d58dddf2c5b85007820ed7
Emanuele Galli <egalli64@gmail.com>
2021-09-01 13:06:00 +0200

    more examples

--- src/main/java/m11/s10/ReduceExample.java
+++ src/main/java/m11/s10/ReduceExample.java
@@ -1,30 +1,37 @@
 package m11.s10;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ReduceExample {
     public static void main(String[] args) {
-        List<Integer> data = List.of(4, 2, 5, 23, 54, 11, 67, 100, 15, 83, 45);
-        System.out.println(data);
+        int[] rawData = {4, 2, 5, 23, 54, 11, 67, 100, 15, 83, 45};
+        List<Integer> data = IntStream.of(rawData).boxed().collect(Collectors.toList());
+        System.out.println("Working on: " + data);
 
-        Integer sum = data.stream().reduce(0, (x, y) -> x + y);
-        System.out.println("Adding up elements explicitly: " + sum);
+        System.out.println("Adding up elements explicitly: " +
+                data.stream().reduce(0, (x, y) -> x + y));
 
-        data.stream().reduce(Integer::sum) //
+        data.stream().reduce(Integer::sum)
                 .ifPresent(z -> System.out.println("Adding up elements by sum: " + z));
 
-        data.stream().reduce(Integer::max) //
+        Arrays.stream(rawData).reduce(Integer::sum)
+                .ifPresent(z -> System.out.println("Raw version: " + z));
+
+        data.stream().reduce(Integer::max)
                 .ifPresent(z -> System.out.println("Largest value by max: " + z));
 
-        data.stream().reduce(Integer::min) //
+        data.stream().reduce(Integer::min)
                 .ifPresent(z -> System.out.println("Smallest value by min: " + z));
 
         // map-reduce example
-        data.stream().map(x -> x % 2 == 0 ? 0 : 1).reduce(Integer::sum) //
+        data.stream().map(x -> x % 2 == 0 ? 0 : 1).reduce(Integer::sum)
                 .ifPresent(z -> System.out.println("Count of odd numbers by map-reduce: " + z));
 
         // in this last case, using map-reduce is an overkill
-        System.out.println("Count of odd numbers by count: " + //
+        System.out.println("Count of odd numbers by count: " +
                 data.stream().filter(x -> x % 2 == 1).count());
     }
 }
