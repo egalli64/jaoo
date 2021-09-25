066ab4bff8b093d4ab766922615901703b1ca3cc
Emanuele Galli <egalli64@gmail.com>
2021-09-25 10:09:57 +0200

    minor cleanup

--- src/main/java/m05/s05/Wrapper.java
+++ src/main/java/m05/s05/Wrapper.java
@@ -4,22 +4,27 @@ import java.util.Arrays;
 
 public class Wrapper {
     public static void main(String[] args) {
-        Object[] wrappers = { Boolean.class, Character.class, Short.class, Integer.class, Float.class, Double.class };
+        Object[] wrappers = {Boolean.class, Character.class, Short.class, Integer.class, Float.class, Double.class};
         System.out.println("Wrapper classes: " + Arrays.toString(wrappers));
 
-        // Ctor deprecated from Java 9 on
-        @SuppressWarnings("deprecation")
+        // deprecated for removal since Java 9
+        @SuppressWarnings("removal")
         Integer i = new Integer(1);
+
         Integer j = Integer.valueOf(2);
-        System.out.println(String.format("%d plus %d is %d", i, j, i + j));
+        System.out.printf("%d plus %d is %d%n", i, j, i + j);
 
         int k = j.intValue();
         System.out.println("k is " + k);
 
         Integer m = 3;
-        System.out.println("m is " + m);
+        f(m);
 
         int n = j;
         System.out.println("n is " + n);
+    }
+
+    private static void f(Object obj) {
+        System.out.println("An object: " + obj);
     }
 }
