6c0d66d3769ddaefcb25e1c925b64b54310ad5a0
Emanuele <egalli64@gmail.com>
2021-10-09 17:04:12 +0200

    split assertj

--- src/test/java/m04/s06/SimpleTest.java
+++ src/test/java/m04/s06/SimpleTest.java
@@ -1,10 +1,10 @@
 package m04.s06;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.withPrecision;
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class SimpleTest {
     @Test
@@ -31,58 +31,6 @@ class SimpleTest {
         double expected = .87;
         double actual = .29 * 3;
         assertEquals(expected, actual, .0001);
-    }
-
-    @Test
-    void ajTrue() {
-        boolean actual = isPositive(42);
-        assertThat(actual).isTrue();
-    }
-
-    @Test
-    void ajEqTrue() {
-        boolean actual = isPositive(42);
-        assertThat(actual).isEqualTo(true);
-    }
-
-    @Test
-    void ajNull() {
-        String actual = nullIfPositive(12);
-        assertThat(actual).isNull();
-    }
-
-    @Test
-    void ajEqNull() {
-        String actual = nullIfPositive(42);
-        assertThat(actual).isEqualTo(null);
-    }
-
-    @Test
-    void ajEquals() {
-        int expected = 42;
-        int actual = 42;
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    void ajEqualsDouble() {
-        double expected = .87;
-        double actual = .29 * 3;
-        assertThat(actual).isEqualTo(expected, withPrecision(4d));
-    }
-
-    @Test
-    void ajStart() {
-        String prefix = "Tom";
-        String actual = "Tom Jones";
-        assertThat(actual).startsWith(prefix);
-    }
-
-    @Test
-    void ajNotStart() {
-        String prefix = "Bob";
-        String actual = "Tom Jones";
-        assertThat(actual).doesNotStartWith(prefix);
     }
 
     private boolean isPositive(int value) {
---
+++ src/test/java/m04/s06/SomethingElseTest.java
@@ -0,0 +1,75 @@
+package m04.s06;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
+
+import org.junit.jupiter.api.Test;
+
+class SomethingElseTest {
+    @Test
+    void ajTrue() {
+        boolean actual = isPositive(42);
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void ajEqTrue() {
+        boolean actual = isPositive(42);
+        assertThat(actual).isEqualTo(true);
+    }
+
+    @Test
+    void ajNull() {
+        String actual = nullIfPositive(12);
+        assertThat(actual).isNull();
+    }
+
+    @Test
+    void ajEqNull() {
+        String actual = nullIfPositive(42);
+        assertThat(actual).isEqualTo(null);
+    }
+
+    @Test
+    void ajEquals() {
+        int expected = 42;
+        int actual = 42;
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void ajDoubleClose() {
+        double expected = .87;
+        double actual = .29 * 3;
+        assertThat(actual).isCloseTo(expected, withPrecision(.000_001));
+    }
+
+    @Test
+    void ajDoubleNotClose() {
+        double expected = .87;
+        double actual = .29 * 3;
+        assertThat(actual).isNotCloseTo(expected, withPrecision(.000_000_000_000_000_1));
+    }
+
+    @Test
+    void ajStart() {
+        String prefix = "Tom";
+        String actual = "Tom Jones";
+        assertThat(actual).startsWith(prefix);
+    }
+
+    @Test
+    void ajNotStart() {
+        String prefix = "Bob";
+        String actual = "Tom Jones";
+        assertThat(actual).doesNotStartWith(prefix);
+    }
+
+    private boolean isPositive(int value) {
+        return value > 0;
+    }
+
+    private String nullIfPositive(int value) {
+        return value > 0 ? null : "not a null";
+    }
+}
