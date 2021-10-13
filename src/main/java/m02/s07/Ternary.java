3aa146eb2482a9aacba549e1500e6f0271978ff8
Emanuele <egalli64@gmail.com>
2021-10-13 18:05:22 +0200

    Element index fixed

--- src/main/java/m02/s07/Ternary.java
+++ src/main/java/m02/s07/Ternary.java
@@ -2,14 +2,14 @@ package m02.s07;
 
 public class Ternary {
     public static void main(String[] args) {
-        String version1 = args.length == 0 ? "No arg passed" : args[1];
+        String version1 = args.length == 0 ? "No arg passed" : args[0];
         System.out.println(version1);
 
         String version2;
         if (args.length == 0) {
             version2 = "No arg passed";
         } else {
-            version2 = args[1];
+            version2 = args[0];
         }
         System.out.println(version2);
     }
