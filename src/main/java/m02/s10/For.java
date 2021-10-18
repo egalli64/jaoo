36dbb677271a5f0d1102cf573b33117763d597bf
Emanuele <egalli64@gmail.com>
2021-10-18 18:56:35 +0200

    Readability

--- src/main/java/m02/s10/For.java
+++ src/main/java/m02/s10/For.java
@@ -4,14 +4,14 @@ public class For {
     public static void main(String[] args) {
         System.out.println("Please, pass a few args");
         for (int i = 0; i < args.length; i++) {
-            System.out.print("Arg " + i);
+            System.out.print("The argument " + i);
 
             if (i == 2) {
-                System.out.println(" skipped");
+                System.out.println(" is skipped");
                 continue;
             }
 
-            System.out.println(": " + args[i]);
+            System.out.println(" is " + args[i]);
         }
 
         System.out.println("done!");
