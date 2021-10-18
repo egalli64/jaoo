697451c7b933a1ba88802777938325e2c4021b71
Emanuele <egalli64@gmail.com>
2021-10-18 18:53:39 +0200

    more comments

--- src/main/java/m02/s04/Concatenation.java
+++ src/main/java/m02/s04/Concatenation.java
@@ -2,16 +2,23 @@ package m02.s04;
 
 public class Concatenation {
     public static void main(String[] args) {
+        // add
         System.out.print("5 + 7: ");
-        System.out.println(5 + 7);
+        System.out.println(5 + 7); // 12
 
+        // cast to string and concatenate
         System.out.print("5 + \"7\": ");
-        System.out.println(5 + "7");
+        System.out.println(5 + "7"); // 57
+
+        // add, cast to string and concatenate
+        System.out.print("5 + 7 + \"4\": ");
+        System.out.println(5 + 7 + "4"); // -> 124
 
         System.out.println("Resistence" + " is " + "useless");
         System.out.println("Solution: " + 42);
         System.out.println(true + " or " + false);
 
+        // "multiply" a string
         System.out.print("Repeat \"Vogons\" three times: ");
         System.out.println("Vogons".repeat(3)); // Java 11
 
