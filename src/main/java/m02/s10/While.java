da8e8fb30aa26cd8dc60758ede5d2f32270dd24d
Emanuele <egalli64@gmail.com>
2021-11-06 12:55:16 +0100

    more comments

--- src/main/java/m02/s10/While.java
+++ src/main/java/m02/s10/While.java
@@ -4,8 +4,21 @@ public class While {
     public static void main(String[] args) {
         int i = 0;
         while (i < args.length) {
-            System.out.println("Arg " + i + ": " + args[i]);
+            System.out.println("The argument " + i + " is " + args[i]);
+
+            // same as above, more verbosely, see also StringBuilder
+            String s = "The argument ";
+            s = s + i;
+            s = s + " is ";
+            s = s + args[i];
+            System.out.println(s);
+
             i += 1;
+
+            // other ways to increase a number (no side effect here!)
+            // i++;
+            // ++i;
+            // i = i + 1;
         }
 
         System.out.println("done!");
