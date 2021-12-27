ce046e9e91f290b1e9102fc4ed88864ed1c4debe
Emanuele <egalli64@gmail.com>
2021-12-27 16:57:46 +0100

    unused import

--- src/main/java/com/example/jse/m11/s04/caster/Main.java
+++ src/main/java/com/example/jse/m11/s04/caster/Main.java
@@ -1,12 +1,7 @@
 package com.example.jse.m11.s04.caster;
 
-import java.util.Arrays;
-import java.util.concurrent.FutureTask;
-import java.util.stream.LongStream;
-
 public class Main {
     private static final int NR = 3;
-    private static final int MAX_VALUE = 6;
 
     public static void main(String[] args) {
         Worker[] workers = new Worker[NR];
