e604261cf07aa1b46eb913d1f39f2cf2daea83e8
Emanuele <egalli64@gmail.com>
2022-02-17 10:37:36 +0100

    simplify

---
+++ src/main/java/com/example/jse/m11/s03/Main.java
@@ -0,0 +1,16 @@
+package com.example.jse.m11.s03;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Running thread %s%n", Thread.currentThread().getName());
+
+        System.out.printf("About to sleep @ %d%n", System.currentTimeMillis());
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            System.out.println("Sleep interrupted: " + e.getMessage());
+        }
+
+        System.out.printf("...sleep ended @ %d%n", System.currentTimeMillis());
+    }
+}
