cd8eb60bae775bd6666e963ad3b4be7bc88a844d
Emanuele <egalli64@gmail.com>
2022-02-20 11:52:33 +0100

    slide moved

---
+++ src/main/java/com/example/jse/m11/s10/CompletableFuture2TSupply.java
@@ -0,0 +1,32 @@
+package com.example.jse.m11.s10;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+public class CompletableFuture2TSupply {
+    public static void main(String[] args) {
+        System.out.println("Create and start the completable future task");
+        Future<String> greeter = CompletableFuture.supplyAsync( () -> {
+            try {
+                Thread.sleep(1_000);
+                return args.length > 0 ? args[0] : "Hello!";
+            } catch (InterruptedException e) {
+                return "Interrupted task";
+            }
+        });
+
+        if (greeter.isDone()) {
+            System.out.println("Unexpected, the greeter should take some time to complete!");
+        } else {
+            System.out.println("While the greeter works, do something else in the main thread");
+        }
+
+        System.out.println("When there is nothing more to do, wait the greeter to complete");
+        try {
+            System.out.println(greeter.get());
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
