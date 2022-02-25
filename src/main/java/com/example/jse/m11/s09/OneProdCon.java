b005a423555f72b309d80ed57a5d4318e1fd2624
Emanuele <egalli64@gmail.com>
2022-02-25 09:31:52 +0100

    review

--- src/main/java/com/example/jse/m11/s09/OneProdCon.java
+++ src/main/java/com/example/jse/m11/s09/OneProdCon.java
@@ -5,27 +5,29 @@ public class OneProdCon {
     private int result = 0;
 
     private synchronized void producer() {
+        String tName = Thread.currentThread().getName();
         try {
-            System.out.println("Producer is working on a (simulated) long job");
+            System.out.println(tName + " is working on a (simulated) long job");
             Thread.sleep(500);
         } catch (InterruptedException e) {
             throw new IllegalStateException(e);
         }
 
         result = BASE_VALUE;
-        System.out.println("Producer produces as result " + result);
+        System.out.printf("%s has produced as result %d%n", tName, result);
         notifyAll();
     }
 
     private synchronized void consumer() {
+        String tName = Thread.currentThread().getName();
         try {
             while (result == 0) {
-                System.out.println("Consumer waits for the result");
+                System.out.println(tName + " waits for the result");
                 wait();
-                System.out.println("Consumer wait is ended");
+                System.out.println(tName + " wait is ended");
             }
 
-            System.out.println("Consumer consumes " + result);
+            System.out.printf("%s consumes %d%n", tName, result);
             result = 0;
         } catch (InterruptedException e) {
             throw new IllegalStateException(e);
@@ -35,8 +37,19 @@ public class OneProdCon {
     public static void main(String[] args) {
         OneProdCon wn = new OneProdCon();
 
-        new Thread(wn::producer).start();
-        wn.consumer();
+        Thread ts[] = { new Thread(wn::consumer, "consumer"), new Thread(wn::producer, "producer") };
+
+        for (Thread t : ts) {
+            t.start();
+        }
+
+        for (Thread t : ts) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        }
 
         System.out.println("Bye");
     }
