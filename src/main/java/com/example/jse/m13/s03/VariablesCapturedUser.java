cd2fe43932a36fae5cfdc48c7cbc2be272aa2f99
Emanuele <egalli64@gmail.com>
2022-03-17 15:59:16 +0100

    moved to jfp and jast

--- src/main/java/com/example/jse/m13/s03/VariablesCapturedUser.java
+++
@@ -1,8 +0,0 @@
-package com.example.jse.m13.s03;
-
-public class VariablesCapturedUser {
-    public static void executor(Runnable runnable) {
-        System.out.print("The executor receives a lambda and run it: ");
-        runnable.run();
-    }
-}
--- src/main/java/com/example/jse/m13/s14/Weight.java
+++
@@ -1,5 +0,0 @@
-package com.example.jse.m13.s14;
-
-enum Weight {
-    LIGHT, MEDIUM, HEAVY
-}
