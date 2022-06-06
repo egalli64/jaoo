4b015887a2fe23bcd97966febb2b676c87f7229d
Emanuele <egalli64@gmail.com>
2022-06-06 09:42:24 +0200

    cleanup

--- src/main/java/com/example/jse/m02/s12/While.java
+++ src/main/java/com/example/jse/m02/s12/While.java
@@ -2,25 +2,16 @@ package com.example.jse.m02.s12;
 
 public class While {
     public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("The argument " + i + " is " + args[i]);
+        }
+
         int i = 0;
         while (i < args.length) {
             System.out.println("The argument " + i + " is " + args[i]);
-
-            // same as above, more verbosely, see also StringBuilder
-            String s = "The argument ";
-            s = s + i;
-            s = s + " is ";
-            s = s + args[i];
-            System.out.println(s);
-
-            i += 1;
-
-            // other ways to increase a number (no side effect here!)
-            // i++;
-            // ++i;
-            // i = i + 1;
+            i++;
         }
 
-        System.out.println("done!");
+        System.out.println("done, looped " + i + " times");
     }
 }
