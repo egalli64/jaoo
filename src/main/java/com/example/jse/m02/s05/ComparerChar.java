0b9a7f88089f23481236cab0aef3291555fc4185
Emanuele <egalli64@gmail.com>
2022-06-06 09:00:40 +0200

    more comments

--- src/main/java/com/example/jse/m02/s05/ComparerChar.java
+++ src/main/java/com/example/jse/m02/s05/ComparerChar.java
@@ -2,15 +2,19 @@ package com.example.jse.m02.s05;
 
 public class ComparerChar {
     public static void main(String[] args) {
+        // letters are coded in the expected order
         System.out.print("'c' < 'k'? ");
         System.out.println('c' < 'k');
 
-        System.out.print("'A' < 'a'? ");
-        System.out.println('A' < 'a');
+        // all uppercase letters are before all lowercase letters
+        System.out.print("'Z' < 'a'? ");
+        System.out.println('Z' < 'a');
 
+        // ciphers are coded in the expected order
         System.out.print("'0' < '9'? ");
         System.out.println('0' < '9');
 
+        // beware of characters that look similar!
         System.out.print("'ì' == 'i'? ");
         System.out.println('ì' == 'i');
     }
