be05fac2e0306f8d8ce22f94fc555e05f0178eaa
Emanuele <egalli64@gmail.com>
2022-06-06 10:07:33 +0200

    cipher checker

---
+++ src/main/java/com/example/jse/m05/s06/Exercise.java
@@ -0,0 +1,14 @@
+package com.example.jse.m05.s06;
+
+public class Exercise {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.out.println("Pass me one argument!");
+        } else {
+            // TODO (1) check if the argument contains only ciphers
+            // TODO (2) check if the argument contains at least one cipher
+
+            System.out.println("Done");
+        }
+    }
+}
