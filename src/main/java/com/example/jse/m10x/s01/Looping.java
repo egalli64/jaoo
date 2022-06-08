254c5fd115c55ef23d6959d0df080fd6ba14f292
Emanuele <egalli64@gmail.com>
2022-06-08 10:43:25 +0200

    more on collection

---
+++ src/main/java/com/example/jse/m10x/s01/Looping.java
@@ -0,0 +1,21 @@
+package com.example.jse.m10x.s01;
+
+import java.util.Collection;
+import java.util.List;
+
+public class Looping {
+    public static void main(String[] args) {
+        Collection<Integer> iterable = List.of(1, 3, 4, 2, 42);
+        System.out.println("Collection: " + iterable);
+
+        System.out.print("for-each loop: ");
+        for (Integer value : iterable) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+
+        System.out.print("Java 8 forEach(): ");
+        iterable.forEach(value -> System.out.print(value + " "));
+        System.out.println();
+    }
+}
