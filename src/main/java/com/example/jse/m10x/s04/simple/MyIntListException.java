115effad9fd4f064e6220ec5c492ef8b535385ef
Emanuele <egalli64@gmail.com>
2022-06-11 11:07:53 +0200

    moved classes

--- src/main/java/com/example/jse/m10x/s04/simple/MyIntListException.java
+++
@@ -1,7 +0,0 @@
-package com.example.jse.m10x.s04.simple;
-
-public class MyIntListException extends Exception {
-    private static final long serialVersionUID = 1L;
-
-    // TODO: ctor accepting a message
-}
