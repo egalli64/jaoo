23ad20c3d3edc696314c5ff1285ee5fb75c408fc
Emanuele <egalli64@gmail.com>
2022-09-10 10:44:46 +0200

    more details

--- src/main/java/com/example/jse/m01/s07/Narrowing.java
+++ src/main/java/com/example/jse/m01/s07/Narrowing.java
@@ -2,18 +2,26 @@ package com.example.jse.m01.s07;
 
 public class Narrowing {
     public static void main(String[] args) {
-        double aDouble = 42;
+        double aDouble = 42.0;
         float aFloat = (float) aDouble;
+
+        // [0000 0000][0000 0000][0000 0000][0000 0000][0000 0000][0000 0000][0000 0000][0010 1010]
         long aLong = (long) aFloat;
+
         // [0000 0000][0000 0000][0000 0000][0010 1010]
         int anInt = (int) aLong;
+
+        // [0000 0000][0010 1010]
         short aShort = (short) anInt;
+
         // [00101010]
         byte aByte = (byte) aShort;
 
         System.out.println(aByte);
 
+        // [0000 0000][0010 1010]
         char aChar = (char) anInt;
+
         short anotherShort = (short) aChar;
 
         // notice the difference between print() and println()
