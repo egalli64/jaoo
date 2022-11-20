edb90e8aedb9f0d76f439c35a2bc3afab942500d
Emanuele <egalli64@gmail.com>
2022-11-20 20:54:52 +0100

    more comments

--- src/main/java/com/example/jse/m01/s04/Simple.java
+++ src/main/java/com/example/jse/m01/s04/Simple.java
@@ -1,14 +1,21 @@
 /*
  * A simple Java source file
+ * 
+ * https://github.com/egalli64/jse
  */
 package com.example.jse.m01.s04;
 
 /**
- * A "hello" class
+ * A "hello" program that uses the passed args
  * 
  * @author Emanuele Galli
  */
 public class Simple {
+    /**
+     * Print hello for the passed unique arg (or generic hello)
+     * 
+     * @param args the user passed args, expected just 0 or 1 of them
+     */
     public static void main(String[] args) {
         // output a string to console
         System.out.print("Hello, ");
