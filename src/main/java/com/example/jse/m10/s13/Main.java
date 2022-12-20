4f6c632efbdd958623438f8abad337d7a3374eac
Emanuele <egalli64@gmail.com>
2022-12-20 18:02:01 +0100

    minor cleanup

--- src/main/java/com/example/jse/m10/s13/Main.java
+++ src/main/java/com/example/jse/m10/s13/Main.java
@@ -1,8 +1,16 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.m10.s13;
 
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Example on generic classes
+ */
 public class Main {
     public static void main(String[] args) {
         List<Integer> li = new ArrayList<Integer>();
@@ -14,39 +22,53 @@ public class Main {
         noGenericArray();
     }
 
-    private static void noGenericTypeCheck(List<Integer> li) {
-        // Cannot perform instanceof check against parameterized type
-//        if (li instanceof List<Integer>) {
+    /**
+     * Cannot perform instanceof check against parameterized type, only against
+     * unbounded one
+     * 
+     * @param obj
+     */
+    private static void noGenericTypeCheck(Object obj) {
+//        if (obj instanceof List<Integer>) {
 //            System.out.println("As expected");
 //        }
 
-        if (li instanceof List<?>) {
-            System.out.println("It is a List (of what?)");
+        if (obj instanceof List) {
+            List<?> li = (List<?>) obj;
+            System.out.print("The passed object is a List (of what?)");
+            System.out.println(" sized " + li.size());
         }
     }
 
+    /**
+     * Cast of a parametric type only to unbounded type
+     */
     private static void noGenericDowncast() {
         List<Animal> la = new ArrayList<>();
         la.add(new Dog());
-        // Cannot cast
+
 //        ArrayList<Dog> ald = (ArrayList<Dog>) la;
 
         ArrayList<?> alo = (ArrayList<?>) la;
         System.out.println("MyArray contains " + alo.size() + " object(s)");
     }
 
+    /**
+     * Cast of a parametric type only to unbounded type
+     */
     private static void noGenericUpcast() {
         List<Dog> ld = new ArrayList<>();
         ld.add(new Dog());
-        // Cannot cast
-//        ArrayList<Animal> ala = (ArrayList<Animal>) ld;
+        // ArrayList<Animal> ala = (ArrayList<Animal>) ld;
 
         ArrayList<?> alo = (ArrayList<?>) ld;
         System.out.println("MyArray contains " + alo.size() + " object(s)");
     }
 
+    /**
+     * Cannot create an array of generics
+     */
     private static void noGenericArray() {
-        // Cannot create an array of generics
 //        List<Dog>[] ld = new ArrayList<Dog>[10];
 
         // If you really need it
