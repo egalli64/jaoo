9c999f9b10d8c29e92bbf2f99d6f09bea0369263
Emanuele <egalli64@gmail.com>
2022-12-20 12:39:32 +0100

    improved immutable class

--- src/main/java/com/example/jse/m06/s16/Dog.java
+++ src/main/java/com/example/jse/m06/s16/Dog.java
@@ -1,5 +1,28 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.m06.s16;
 
-public class Dog {
+/**
+ * An immutable class
+ */
+public final class Dog {
     public static final int HOUSING_COST = 10;
+
+    private final String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public void bark() {
+        System.out.println(name + ": bark!");
+    }
+
+    @Override
+    public String toString() {
+        return "Dog [name=" + name + "]";
+    }
 }
