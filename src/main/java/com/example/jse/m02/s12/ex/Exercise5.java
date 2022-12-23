05e44845f2b9a0e2a91cca30d3369b078f8e45d0
Emanuele <egalli64@gmail.com>
2022-12-23 12:52:17 +0100

    loops and arithmetic operators

---
+++ src/main/java/com/example/jse/m02/s12/ex/Exercise5.java
@@ -0,0 +1,20 @@
+package com.example.jse.m02.s12.ex;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+
+        int multi = a * b;
+        System.out.println("a * b = " + multi);
+        // TODO (1) Get the same result without multiplying
+
+        int div = a / b;
+        System.out.println("a / b = " + div);
+        // TODO (2) Get the same result without dividing
+
+        int mod = a % b;
+        System.out.println("a % b = " + mod);
+        // TODO (3) Get the same result without using the modulo operator
+    }
+}
