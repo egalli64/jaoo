9dc4bb3d8bb25870844eb7ab79262ac90aa98f69
Emanuele <egalli64@gmail.com>
2023-01-03 23:03:28 +0100

    review, cleanup, more info

--- src/main/java/com/example/jse/mex/s02/Exercise1.java
+++ src/main/java/com/example/jse/mex/s02/Exercise1.java
@@ -1,16 +1,29 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.mex.s02;
 
 import java.util.logging.Logger;
 
+import com.example.jse.mex.ExerciseUncheckedException;
+
+/**
+ * Operators - Exercise 1
+ * 
+ * Dividing floating point values
+ */
 public class Exercise1 {
     private static final Logger log = Logger.getGlobal();
 
     /**
-     * Calculate the average speed
+     * Calculate the average speed (it is very simple, just a division)
      * 
      * @param distance in meters
      * @param time     in seconds
      * @return speed in meters per second
+     * @throws ExerciseUncheckedException (optional) in case of negative time
      */
     public static double speed(double distance, double time) {
         log.info(String.format("distance is %.3f and time is %.2f", distance, time));
--- src/test/java/com/example/jse/mex/s02/Exercise1Test.java
+++ src/test/java/com/example/jse/mex/s02/Exercise1Test.java
@@ -1,49 +1,96 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.mex.s02;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.withPrecision;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.example.jse.mex.ExerciseUncheckedException;
 
+/**
+ * Operators - Exercise 1
+ * 
+ * Dividing floating point values
+ */
 class Exercise1Test {
+    /**
+     * 100 meters in 9.58 seconds means an _average_ speed of 10.4384... m/s
+     * <p>
+     * 100m World Record set by Usain Bolt (2009)
+     */
     @Test
-    void speedPlain() {
+    void speedPlainJupiterOnly() {
         double actual = Exercise1.speed(100.0, 9.58);
         double expected = 10.438;
 
-        assertEquals(expected, actual, .0001);
+        Assertions.assertEquals(expected, actual, .001);
     }
 
+    /**
+     * Duplicated test to show the AssertJ approach
+     */
     @Test
-    void speedPlainAJ() {
+    void speedPlainWithAssertJ() {
         double actual = Exercise1.speed(100.0, 9.58);
         double expected = 10.438;
 
-        assertThat(actual).isEqualTo(expected, withPrecision(.0001));
+        assertThat(actual).isEqualTo(expected, withPrecision(.001));
     }
 
+    /**
+     * 100 meters in 0.0 seconds means positive infinity velocity
+     */
     @Test
-    void speedInfinity() {
+    void speedPositiveInfinity() {
         double actual = Exercise1.speed(100.0, 0.0);
         double expected = Double.POSITIVE_INFINITY;
 
         assertThat(actual).isEqualTo(expected);
     }
 
+    /**
+     * -100 meters in 0.0 seconds means negative infinity velocity
+     */
     @Test
-    @Disabled("enable it for an exception throwing version")
-    void speedNegativeTime() {
-        try {
-            Exercise1.speed(100, -1);
-            fail("An ExerciseUncheckedException was expected");
-        } catch (ExerciseUncheckedException eue) {
-            String message = eue.getMessage();
-            assertThat(message).isEqualTo("No negative values expected");
-        }
+    void speedNegativeInfinity() {
+        double actual = Exercise1.speed(-100.0, 0.0);
+        double expected = Double.NEGATIVE_INFINITY;
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    /**
+     * Negative time should lead to an ExerciseUncheckedException with message "Time
+     * should be non-negative"
+     * <p>
+     * Enable it when you know about exceptions
+     */
+    @Test
+    @Disabled("enable it for testing exceptional cases")
+    void speedNegativeTimeJupiterOnly() {
+        ExerciseUncheckedException ex = Assertions.assertThrows( //
+                ExerciseUncheckedException.class, () -> Exercise1.speed(100, -1));
+        Assertions.assertEquals("Time should be non-negative", ex.getMessage());
+    }
+
+    /**
+     * Duplicated test to show the AssertJ approach
+     * <p>
+     * Enable it when you know about exceptions
+     */
+    @Test
+    @Disabled("enable it for testing exceptional cases")
+    void speedNegativeTimeWithAssertJ() {
+        assertThatThrownBy(() -> Exercise1.speed(100, -1)) //
+                .isInstanceOf(ExerciseUncheckedException.class) //
+                .hasMessage("Time should be non-negative");
     }
 }
