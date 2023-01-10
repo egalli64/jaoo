6bfa722881c870b6f9c3d5c211f71967d0a2cc26
Emanuele <egalli64@gmail.com>
2023-01-10 09:12:23 +0100

    more comments

--- src/main/java/com/example/jse/mex/s02/Exercise4.java
+++ src/main/java/com/example/jse/mex/s02/Exercise4.java
@@ -1,5 +1,15 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.mex.s02;
 
+/**
+ * Operators - Exercise 4
+ * 
+ * Fahrenhein is 32 plus 9/5 of Celsius
+ */
 public class Exercise4 {
     /**
      * Celsius to Fahrenheit converter
--- src/test/java/com/example/jse/mex/s02/Exercise4Test.java
+++ src/test/java/com/example/jse/mex/s02/Exercise4Test.java
@@ -1,3 +1,8 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.mex.s02;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -5,20 +10,45 @@ import static org.assertj.core.api.Assertions.withPrecision;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Operators - Exercise 4
+ * 
+ * Get Fahrenhein from Celsius
+ */
 class Exercise4Test {
+    /**
+     * 0.0 Celsius is exactly 32.0 Fahrenhein
+     */
     @Test
     void testZeroCelsius() {
-        double actual = Exercise4.toFahrenhein(0);
-        double expected = 32;
+        final double celsius = 0.0;
+        final double actual = Exercise4.toFahrenhein(celsius);
+        final double expected = 32.0;
 
-        assertThat(actual).isEqualTo(expected, withPrecision(6d));
+        assertThat(actual).isEqualTo(expected);
     }
 
+    /**
+     * -40.0 Celsius is exactly -40.0 Fahrenhein
+     */
+    @Test
+    void testMinus40Celsius() {
+        final double celsius = -40.0;
+        final double actual = Exercise4.toFahrenhein(celsius);
+        final double expected = -40.0;
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    /**
+     * 0.0 Fahrenhein is pretty close to -17.777_777 Celsius
+     */
     @Test
     void testZeroFahrenheit() {
-        double actual = Exercise4.toFahrenhein(-17.777_778);
-        double expected = 0;
+        final double celsius = -17.777_777;
+        final double actual = Exercise4.toFahrenhein(celsius);
+        final double expected = 0;
 
-        assertThat(actual).isEqualTo(expected, withPrecision(6d));
+        assertThat(actual).isEqualTo(expected, withPrecision(0.000_002));
     }
 }
