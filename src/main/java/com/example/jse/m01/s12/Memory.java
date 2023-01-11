2e273384d8acef991f5485ea3c19a01b1abb889f
Emanuele <egalli64@gmail.com>
2023-01-11 12:08:34 +0100

    more comments

--- src/main/java/com/example/jse/m01/s12/Memory.java
+++ src/main/java/com/example/jse/m01/s12/Memory.java
@@ -1,12 +1,29 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.m01.s12;
 
 import java.util.Arrays;
 
+/**
+ * Stack vs Heap
+ */
 public class Memory {
+    /**
+     * A primitive could be on the stack or on the heap (inside an object). An
+     * object could only be on the heap. A reference to an object could be on the
+     * stack or on the heap (inside an object).
+     * 
+     * @param args not used
+     */
     public static void main(String[] args) {
+        // primitive, on the stack
         int x = 12;
         System.out.println(x);
 
+        // reference, the object is on the heap, it contains primitive values
         int values[] = { 7, x };
         System.out.println(Arrays.toString(values));
 
@@ -16,6 +33,7 @@ public class Memory {
         int data[] = new int[3];
         System.out.println(Arrays.toString(data));
 
+        // reference, the object is on the heap, it contains references to other objects
         String[] names = { "Bo", s };
         System.out.println(Arrays.toString(names));
     }
