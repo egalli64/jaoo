fabf15bb774507d9f3d692bc4cb30121b6a81daf
Emanuele <egalli64@gmail.com>
2023-01-13 17:52:08 +0100

    more comments

--- src/main/java/com/example/jse/m03/s04/ErrorCode.java
+++ src/main/java/com/example/jse/m03/s04/ErrorCode.java
@@ -1,12 +1,27 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.m03.s04;
 
+/**
+ * Bad arguments - signaled by error code
+ */
 public class ErrorCode {
+    /**
+     * When calling Math::sqrt() we should remember to check the returned value (if
+     * we are not dead sure about the argument). Otherwise we could have an
+     * unexpected NaN in a variable.
+     * 
+     * @param args not used
+     */
     public static void main(String[] args) {
-        // Bad argument
+        // a bad argument
         double argument = -1.0;
         double radix = Math.sqrt(argument);
 
-        // Here the coder does not check the return value before using it!
+        // We should not use the returned value without checking it!
         System.out.println("Square root is " + radix);
 
         // Check the return value before using it!
--- src/main/java/com/example/jse/m03/s04/Exceptional.java
+++ src/main/java/com/example/jse/m03/s04/Exceptional.java
@@ -1,6 +1,22 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.m03.s04;
 
+/**
+ * Bad arguments - signaled by exception
+ * <p>
+ * When the parameter is a primitive, should be checked for having a good value.
+ */
 public class Exceptional {
+    /**
+     * When calling Exceptional::mySqrt we should remember to try-catch the call.
+     * Otherwise we could have an unexpected program termination.
+     * 
+     * @param args not used
+     */
     public static void main(String[] args) {
         // Bad argument
         double argument = -1.0;
@@ -8,15 +24,23 @@ public class Exceptional {
         try {
             double radix = mySqrt(argument);
             System.out.println("Square root is " + radix);
-        } catch (IllegalArgumentException iae) {
+        } catch (IllegalArgumentException ex) {
             System.out.println("Can't calculate the square root of " + argument);
         }
 
-        // If the coder does not catch the exception, the program ends!
+        // If an exception is not caught, the program ends!
         double radix = mySqrt(argument);
         System.out.println("Square root is " + radix);
     }
 
+    /**
+     * A this wrapper around Math::sqrt(). If the parameter is "not good" an
+     * exception is thrown.
+     * 
+     * @param value it should be a non-negative valid number
+     * @return the square root
+     * @throws IllegalArgumentException for negative values and NaN
+     */
     public static double mySqrt(double value) {
         if (value < 0 || Double.isNaN(value)) {
             throw new IllegalArgumentException("Bad input: " + value);
