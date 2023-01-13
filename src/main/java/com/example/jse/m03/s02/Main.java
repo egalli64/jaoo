fe9c6fdb618ffb2e2a3f18965ce2020740e92dae
Emanuele <egalli64@gmail.com>
2023-01-13 12:48:43 +0100

    more comments

--- src/main/java/com/example/jse/m03/s02/Main.java
+++ src/main/java/com/example/jse/m03/s02/Main.java
@@ -1,6 +1,22 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.m03.s02;
 
+/**
+ * Invoking methods
+ * 
+ * @see Simple the referenced class
+ */
 public class Main {
+    /**
+     * Invoke methods from the standard library or a custom class
+     * 
+     * @param args not used
+     * @see Simple a class with methods invoked here
+     */
     public static void main(String[] args) {
         // calling a static method
         double root = Math.sqrt(121.0);
@@ -8,15 +24,15 @@ public class Main {
         // calling an instance method
         System.out.println(root);
 
+        // calling a static method on a custom class
         String aString = Simple.aStaticMethod();
         System.out.println(aString);
 
+        // to call instance methods we need a reference to an object of that class
         Simple reference = new Simple();
-
         int result = reference.anInstanceMethod(7, 6);
         System.out.println(result);
 
-        reference.anotherInstanceMethod(true);
         reference.anotherInstanceMethod(false);
     }
 }
--- src/main/java/com/example/jse/m03/s02/Simple.java
+++ src/main/java/com/example/jse/m03/s02/Simple.java
@@ -1,14 +1,41 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.m03.s02;
 
+/**
+ * Defining methods
+ * 
+ * @see Main a class that invokes methods of this class
+ */
 public class Simple {
+    /**
+     * A static method
+     * 
+     * @return a string
+     */
     static String aStaticMethod() {
         return "Hi";
     }
 
+    /**
+     * An instance method having as parameters two integers and returning an integer
+     * 
+     * @param a a value
+     * @param b another value
+     * @return the product of the two input values
+     */
     int anInstanceMethod(int a, int b) {
         return a * b;
     }
 
+    /**
+     * A void instance method having as parameter a boolean
+     * 
+     * @param flag a value
+     */
     void anotherInstanceMethod(boolean flag) {
         if (flag) {
             System.out.println("Hello");
