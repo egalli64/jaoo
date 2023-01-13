b2f3ea43b217b896b3468efe12249387abadfb92
Emanuele <egalli64@gmail.com>
2023-01-13 21:11:10 +0100

    more comments

--- src/main/java/com/example/jse/m04/s03/Simple.java
+++ src/main/java/com/example/jse/m04/s03/Simple.java
@@ -1,6 +1,22 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.m04.s03;
 
+/**
+ * A stub for a class that has to be developed
+ * 
+ * @see SimpleTest its JUnit test case
+ */
 public class Simple {
+    /**
+     * Method stub, assume we have still to think about how it should work
+     * 
+     * @param value the input
+     * @return currently, always zero
+     */
     public int negate(int value) {
         return 0;
     }
