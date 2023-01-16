1cffdd2b808d49e3f9450abfa995fba6a2cba659
Emanuele <egalli64@gmail.com>
2023-01-16 21:07:25 +0100

    more comments

--- src/main/java/com/example/jse/m06/s12/AbstractClass.java
+++ src/main/java/com/example/jse/m06/s12/AbstractClass.java
@@ -1,8 +1,18 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.m06.s12;
 
+/**
+ * An abstract class. Having an abstract method it can't be concrete.
+ */
 public abstract class AbstractClass {
+    /** an abstract method */
     public abstract void anAbstractMethod();
 
+    /** a concrete method */
     public void aConcreteMethod() {
     }
 }
--- src/main/java/com/example/jse/m06/s12/ConcreteClass.java
+++ src/main/java/com/example/jse/m06/s12/ConcreteClass.java
@@ -1,10 +1,20 @@
+/*
+ * Introduction to Java Standard Edition
+ * 
+ * https://github.com/egalli64/jse
+ */
 package com.example.jse.m06.s12;
 
 // The type Concrete must implement the inherited abstract method
 //public class ConcreteClass extends AbstractClass {
 //}
 
+/**
+ * A class extending an abstract base class. Since it overrides the base
+ * abstract method, the class could be concrete.
+ */
 public class ConcreteClass extends AbstractClass {
+    /** a concrete method overriding an abstract method */
     @Override
     public void anAbstractMethod() {
         System.out.println("not abstract anymore");
