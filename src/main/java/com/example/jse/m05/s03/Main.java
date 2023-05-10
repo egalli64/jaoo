2ad1666a566b028e7f9aad6a688931dcee346940
Emanuele <egalli64@gmail.com>
2023-05-10 21:21:34 +0200

    one more dog

--- src/main/java/com/example/jse/m05/s03/Main.java
+++ src/main/java/com/example/jse/m05/s03/Main.java
@@ -10,17 +10,24 @@ package com.example.jse.m05.s03;
  */
 public class Main {
     public static void main(String[] args) {
+        Dog tom = new Dog("Tom", "Alsatian");
+        tom.bark();
+
         // no constructor for Dog called, we have six null here
         Dog[] dogs = new Dog[6];
 
         // a dog created with a given name
         dogs[0] = new Dog("Bob");
-        for (int i = 1; i < dogs.length - 1; i++) {
+        // put a reference to a dog in the array
+        dogs[1] = tom;
+        for (int i = 2; i < dogs.length - 1; i++) {
             // each time a dog is created with default name and breed
             dogs[i] = new Dog();
         }
         // a dog is created with give name and breed
-        dogs[dogs.length - 1] = new Dog("Bill", "Fox Terrier");
+        String name = "Bill";
+        String breed = "Fox Terrier";
+        dogs[dogs.length - 1] = new Dog(name, breed);
 
         // let all the dogs bark!
         for (Dog dog : dogs) {
