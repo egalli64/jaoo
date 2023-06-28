30d7bd2844ff71ee54bd56fcf005506265496d67
Emanuele <egalli64@gmail.com>
2023-06-28 09:49:32 +0200

    implicit constant

--- src/main/java/com/example/jse/m06/s09/Barking.java
+++ src/main/java/com/example/jse/m06/s09/Barking.java
@@ -8,10 +8,10 @@ package com.example.jse.m06.s09;
 /**
  * The barking classes should implement this interface
  */
-public interface Barking {
+interface Barking {
     /** bark sound level, when not specified otherwise */
     int DEFAULT_SOUND_LEVEL = 42;
 
     /** every barking class should state how its objects bark */
-    public void bark();
+    void bark();
 }
