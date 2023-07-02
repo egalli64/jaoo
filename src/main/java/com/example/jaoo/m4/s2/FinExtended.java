9e54b3148f8d906acacb198a9436b82efab3fb1d
Emanuele <egalli64@gmail.com>
2023-07-02 14:10:53 +0200

    Final own module

--- src/main/java/com/example/jaoo/m4/s2/FinExtended.java
+++ src/main/java/com/example/jaoo/m4/s2/FinExtended.java
@@ -1,9 +1,9 @@
 /*
- * Introduction to Java Standard Edition
+ * Introduction to OOP with Java
  * 
- * https://github.com/egalli64/jse
+ * https://github.com/egalli64/jaoo
  */
-package com.example.jse.m06.s16;
+package com.example.jaoo.m4.s2;
 
 // Cannot override the final method from Sample
 /**
--- src/main/java/com/example/jaoo/m4/s2/Final.java
+++ src/main/java/com/example/jaoo/m4/s2/Final.java
@@ -1,9 +1,9 @@
 /*
- * Introduction to Java Standard Edition
+ * Introduction to OOP with Java
  * 
- * https://github.com/egalli64/jse
+ * https://github.com/egalli64/jaoo
  */
-package com.example.jse.m06.s16;
+package com.example.jaoo.m4.s2;
 
 /**
  * Marking as final primitive, immutable/mutable references
