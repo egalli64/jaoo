dd0020d05d329d49cca61874d4cf74c8c3efa8ae
Emanuele <egalli64@gmail.com>
2023-10-20 15:44:46 +0200

    an immutable class

---
+++ src/main/java/com/example/jaoo/m4/s4/ImmutableDog.java
@@ -0,0 +1,37 @@
+/*
+ * Introduction to OOP with Java
+ * 
+ * https://github.com/egalli64/jaoo
+ */
+package com.example.jaoo.m4.s4;
+
+/**
+ * An immutable class
+ */
+public final class ImmutableDog {
+    // See String as a more interesting example of immutable class
+    private final String name;
+
+    /**
+     * Initialize the state of the object, no changes in future
+     * 
+     * @param name the dog name
+     */
+    public ImmutableDog(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Being name in immutable, it is safe to pass it around
+     * 
+     * @return the dog name
+     */
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "ImmutableDog [name=" + name + "]";
+    }
+}
