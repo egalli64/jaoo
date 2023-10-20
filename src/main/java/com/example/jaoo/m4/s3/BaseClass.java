31c04410a5763d56f11910df3d5cf51e3d0f1aa3
Emanuele <egalli64@gmail.com>
2023-10-20 15:54:36 +0200

    reworked slide

---
+++ src/main/java/com/example/jaoo/m4/s3/BaseClass.java
@@ -0,0 +1,23 @@
+/*
+ * Introduction to OOP with Java
+ * 
+ * https://github.com/egalli64/jaoo
+ */
+package com.example.jaoo.m4.s3;
+
+/**
+ * An abstract class with final methods
+ */
+public abstract class BaseClass {
+    /**
+     * A method that can't be overridden
+     */
+    public final void f() {
+    }
+
+    /**
+     * A method that can't be hidden
+     */
+    public static final void g() {
+    }
+}
---
+++ src/main/java/com/example/jaoo/m4/s3/DerivedClass.java
@@ -0,0 +1,20 @@
+/*
+ * Introduction to OOP with Java
+ * 
+ * https://github.com/egalli64/jaoo
+ */
+package com.example.jaoo.m4.s3;
+
+/**
+ * A subclass with a super with final methods
+ */
+public class DerivedClass extends BaseClass {
+    // Won't compile: Cannot override the final method from BaseClass
+//    public final void f() {
+//    }
+
+    // Won't compile: Cannot override the final method from BaseClass
+    // Actually, the message is not correct, static method can't be overridden
+//    public static final void g() {
+//    }
+}
