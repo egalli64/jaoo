5357c935fb2f343f0c0533dc76ea2eca66227684
Emanuele <egalli64@gmail.com>
2024-02-27 08:24:19 +0100

    exercise

---
+++ src/main/java/com/example/jaoo/m1/s2/ex/Cat.java
@@ -0,0 +1,13 @@
+/*
+ * Introduction to OOP with Java
+ * 
+ * https://github.com/egalli64/jaoo
+ */
+package com.example.jaoo.m1.s2.ex;
+
+/**
+ * Exercise: Complete the Cat class definition
+ */
+public class Cat {
+    // TODO: define the method meow() that prints "meow!" to standard output
+}
---
+++ src/main/java/com/example/jaoo/m1/s2/ex/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Introduction to OOP with Java
+ * 
+ * https://github.com/egalli64/jaoo
+ */
+package com.example.jaoo.m1.s2.ex;
+
+/**
+ * Exercise: create a cat and let it meow
+ */
+public class Main {
+    public static void main(String[] args) {
+        // TODO 1: Define a reference variable Cat referring to a new Cat object
+
+        // TODO 2: Invoke the method meow() on it
+    }
+}
