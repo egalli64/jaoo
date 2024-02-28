0ea58ad334bf31c308f2a5997d2a79b2e1943ded
Emanuele <egalli64@gmail.com>
2024-02-28 08:39:16 +0100

    exercise

---
+++ src/main/java/com/example/jaoo/m1/s4/ex/Cat.java
@@ -0,0 +1,28 @@
+/*
+ * Introduction to OOP with Java
+ * 
+ * https://github.com/egalli64/jaoo
+ */
+package com.example.jaoo.m1.s4.ex;
+
+/**
+ * A sample class with no public constructor, object could be instantiated only
+ * by static factory methods
+ */
+public class Cat {
+    // TODO: Each cat should have a name and a color
+
+    // TODO: Implement a _private_ constructor to initialize the object fields
+
+    // TODO: Implement a static factory method for a cat with a given color and name
+
+    // TODO: Implement a static factory method for a cat with a given color but an
+    // unknown name
+
+    /**
+     * The current cat meows on the console
+     */
+    public void meow() {
+        // TODO: Print name and color to System.out
+    }
+}
---
+++ src/main/java/com/example/jaoo/m1/s4/ex/Main.java
@@ -0,0 +1,22 @@
+/*
+ * Introduction to OOP with Java
+ * 
+ * https://github.com/egalli64/jaoo
+ */
+package com.example.jaoo.m1.s4.ex;
+
+/**
+ * Exercise: create a few cats and let them meow accordingly to requests
+ */
+public class Main {
+    /**
+     * Follow the instructions in the "to do" comments.
+     * 
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        // TODO: create a few cats, some with name and color, some with just a color
+
+        // TODO: each cat should meow
+    }
+}
