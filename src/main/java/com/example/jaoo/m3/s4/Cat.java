9c293f77e6525d0b54f253536b4523bae96edad9
Emanuele <egalli64@gmail.com>
2024-07-12 16:31:10 +0200

    comparable

---
+++ src/main/java/com/example/jaoo/m3/s4/Cat.java
@@ -0,0 +1,13 @@
+/*
+ * Introduction to OOP with Java
+ * 
+ * https://github.com/egalli64/jaoo
+ */
+package com.example.jaoo.m3.s4;
+
+/**
+ * A non-comparable cat
+ */
+public class Cat {
+
+}
---
+++ src/main/java/com/example/jaoo/m3/s4/MainCompare.java
@@ -0,0 +1,42 @@
+/*
+ * Introduction to OOP with Java
+ * 
+ * https://github.com/egalli64/jaoo
+ */
+package com.example.jaoo.m3.s4;
+
+/**
+ * Example for Comparable::compareTo
+ */
+public class MainCompare {
+    /**
+     * Create two comparable objects and compare them, see what happens when
+     * comparing against an object from another class
+     * 
+     * @param args not used
+     */
+    @SuppressWarnings("unlikely-arg-type")
+    public static void main(String[] args) {
+        ComparableDog tomBob = new ComparableDog("Tom", "Bob");
+        ComparableDog tomCarl = new ComparableDog("Tom", "Carl");
+        if (tomBob.compareTo(tomCarl) < 0) {
+            System.out.println("Tom-Bob is before Tom-Carl");
+        } else {
+            System.out.println("Unexpected");
+        }
+
+        Cat kim = new Cat();
+
+        // suppressed warning on comparing cat and dog
+        if (tomBob.equals(kim)) {
+            System.out.println("Unexpected");
+        } else {
+            System.out.println("The compiler warns about an unlikely argument (if it is not suppressed)");
+        }
+
+        // Won't compile: ComparableDog::compareTo() is not applicable to Cat
+//        if (tomBob.compareTo(kim)) {
+//            // ...
+//        }
+    }
+}
