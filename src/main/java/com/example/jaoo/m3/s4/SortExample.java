137cd67225f7320f06ae5694a01dd80271b8b194
Emanuele <egalli64@gmail.com>
2024-07-18 18:45:07 +0200

    sort

---
+++ src/main/java/com/example/jaoo/m3/s4/SortExample.java
@@ -0,0 +1,30 @@
+/*
+ * Introduction to OOP with Java
+ * 
+ * https://github.com/egalli64/jaoo
+ */
+package com.example.jaoo.m3.s4;
+
+import java.util.Arrays;
+
+/**
+ * Sorting array of objects
+ */
+public class SortExample {
+    public static void main(String[] args) {
+        // Arrays.sort() doesn't know how to sort non-comparable objects
+        Cat[] cats = { new Cat(11), new Cat(2), new Cat(8), new Cat(2) };
+        System.out.println("Original order cats: " + Arrays.toString(cats));
+        try {
+            Arrays.sort(cats);
+        } catch (ClassCastException ex) {
+            System.out.println(ex);
+        }
+
+        // ComparableDogs can be sorted
+        ComparableDog[] dogs = { new ComparableDog("Tom", "Carl"), new ComparableDog("Tom", "Bob") };
+        System.out.println("Original order dogs: " + Arrays.toString(dogs));
+        Arrays.sort(dogs);
+        System.out.println("Sorted dogs: " + Arrays.toString(dogs));
+    }
+}
