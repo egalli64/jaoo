bdbcef7ea48b2913c925fbacd27b21de857c71db
Emanuele <egalli64@gmail.com>
2024-07-18 18:34:52 +0200

    cat id

--- src/main/java/com/example/jaoo/m3/s4/Cat.java
+++ src/main/java/com/example/jaoo/m3/s4/Cat.java
@@ -9,5 +9,18 @@ package com.example.jaoo.m3.s4;
  * A non-comparable cat
  */
 public class Cat {
+    private int id;
 
+    public Cat() {
+        this.id = -1;
+    }
+
+    public Cat(int x) {
+        this.id = x;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat " + id;
+    }
 }
