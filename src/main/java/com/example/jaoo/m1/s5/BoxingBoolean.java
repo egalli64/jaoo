9e765fe06eccf50d8acfa551343af610ef98709d
Emanuele <egalli64@gmail.com>
2024-07-25 10:20:36 +0200

    readability

--- src/main/java/com/example/jaoo/m1/s5/BoxingBoolean.java
+++ src/main/java/com/example/jaoo/m1/s5/BoxingBoolean.java
@@ -26,14 +26,14 @@ public class BoxingBoolean {
         boolean flag = false;
 
         // the static factory method valueOf() is the preferred way for boxing
-        Boolean wFlag = Boolean.valueOf(flag);
-        Boolean wFlag2 = Boolean.valueOf("true");
-        Boolean wFlag3 = Boolean.valueOf("whatever else is false");
+        Boolean boxed = Boolean.valueOf(flag);
+        Boolean boxed2 = Boolean.valueOf("true");
+        Boolean boxed3 = Boolean.valueOf("whatever else is false");
 
         // the use of constructor is deprecated for removal since Java 9
         @SuppressWarnings("removal")
-        Boolean wFlag4 = new Boolean(flag);
+        Boolean boxed4 = new Boolean(flag);
 
-        System.out.printf("A few boxed boolean: %b, %b, %b, %b\n", wFlag, wFlag2, wFlag3, wFlag4);
+        System.out.printf("A few boxed boolean: %b, %b, %b, %b\n", boxed, boxed2, boxed3, boxed4);
     }
 }
