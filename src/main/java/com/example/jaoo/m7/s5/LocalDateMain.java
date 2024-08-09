cdbb7ec8b1fcbfa35a91148fea1536f7418da3e7
Emanuele <egalli64@gmail.com>
2024-08-09 09:38:45 +0200

    clarify

--- src/main/java/com/example/jaoo/m7/s5/LocalDateMain.java
+++ src/main/java/com/example/jaoo/m7/s5/LocalDateMain.java
@@ -21,25 +21,25 @@ public class LocalDateMain {
      */
     public static void main(String[] args) {
         // data factories
-        System.out.printf("A date (by 'of' /1): %s%n", LocalDate.of(2023, Month.JANUARY, 30));
-        System.out.printf("A date (by 'of' /2): %s%n", LocalDate.of(2023, 1, 30));
-        System.out.printf("A date (by 'parse'): %s%n", LocalDate.parse("2023-01-30"));
+        System.out.printf("LocalDate.of() /1: %s\n", LocalDate.of(2024, Month.JANUARY, 30));
+        System.out.printf("LocalDate.of() /2: %s\n", LocalDate.of(2024, 1, 30));
+        System.out.printf("LocalDate.parse(): %s\n", LocalDate.parse("2024-01-30"));
 
         LocalDate today = LocalDate.now();
-        System.out.printf("Current date (now): %s%n", today);
+        System.out.printf("LocalDate.now(): %s\n", today);
 
         // data getter
-        System.out.printf("Current date (getter /1): %02d %02d %d%n", today.getDayOfMonth(), today.getMonthValue(),
+        System.out.printf("Using specific getters: %02d %02d %d\n", today.getDayOfMonth(), today.getMonthValue(),
                 today.getYear());
-        System.out.printf("Current date (getter /2): %02d %02d %d%n", today.get(ChronoField.DAY_OF_MONTH),
+        System.out.printf("Using getter w/ argument: %02d %02d %d\n", today.get(ChronoField.DAY_OF_MONTH),
                 today.get(ChronoField.MONTH_OF_YEAR), today.get(ChronoField.YEAR));
 
         // date with
-        System.out.printf("Tomorrow (with): %s%n", today.with(ChronoField.DAY_OF_MONTH, today.getDayOfMonth() + 1));
-        System.out.printf("Same day, next year (with): %s%n", today.withYear(today.getYear() + 1));
+        System.out.printf("New date by with(): %s\n", today.with(ChronoField.DAY_OF_MONTH, today.getDayOfMonth() + 1));
+        System.out.printf("New date by withYear(): %s\n", today.withYear(today.getYear() + 1));
 
         // date plus / minus
-        System.out.printf("Tomorrow (plus): %s%n", today.plus(1, ChronoUnit.DAYS));
-        System.out.printf("Same day, last year (minus): %s%n", today.minus(1, ChronoUnit.YEARS));
+        System.out.printf("New date by plus(): %s\n", today.plus(1, ChronoUnit.DAYS));
+        System.out.printf("New date by minus(): %s\n", today.minus(1, ChronoUnit.YEARS));
     }
 }
