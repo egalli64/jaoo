1c93ccac47502f88e2105d07a8d56638ec6a202e
Emanuele <egalli64@gmail.com>
2024-08-09 09:52:14 +0200

    clarify

--- src/main/java/com/example/jaoo/m7/s5/LocalTimeMain.java
+++ src/main/java/com/example/jaoo/m7/s5/LocalTimeMain.java
@@ -19,15 +19,14 @@ public class LocalTimeMain {
      */
     public static void main(String[] args) {
         // time factories
-        System.out.printf("A time (by 'of'): %s%n", LocalTime.of(11, 15, 24));
-        System.out.printf("A time (parse): %s%n", LocalTime.parse("11:15:24"));
+        System.out.printf("LocalTime.of(): %s\n", LocalTime.of(11, 15, 24));
+        System.out.printf("LocalTime.parse(): %s\n", LocalTime.parse("11:15:24"));
         LocalTime now = LocalTime.now();
-        System.out.printf("Current time (now): %s%n", now.toString());
+        System.out.printf("LocalTime.now(): %s\n", now);
 
         // time getters
-        System.out.printf("Current time (getter /1): %02d %02d %02d%n", now.getHour(), now.getMinute(),
-                now.getSecond());
-        System.out.printf("Current time (getter /2): %02d %02d %02d%n", now.get(ChronoField.HOUR_OF_DAY),
+        System.out.printf("Using specific getters: %02d:%02d:%02d\n", now.getHour(), now.getMinute(), now.getSecond());
+        System.out.printf("Using getter w/ argument: %02d:%02d:%02d\n", now.get(ChronoField.HOUR_OF_DAY),
                 now.get(ChronoField.MINUTE_OF_HOUR), now.get(ChronoField.SECOND_OF_MINUTE));
     }
 }
