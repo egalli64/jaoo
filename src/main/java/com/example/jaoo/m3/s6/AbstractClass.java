35b3484c4b5b413fd954bb372d19458bcde1b734
Emanuele <egalli64@gmail.com>
2025-02-18 21:38:17 +0100

    moved slide

--- src/main/java/com/example/jaoo/m3/s6/AbstractClass.java
+++ src/main/java/com/example/jaoo/m3/s6/AbstractClass.java
@@ -3,7 +3,7 @@
  * 
  * https://github.com/egalli64/jaoo
  */
-package com.example.jaoo.m3.s5;
+package com.example.jaoo.m3.s6;
 
 /**
  * An abstract class. Having an abstract method it can't be concrete.
--- src/main/java/com/example/jaoo/m3/s6/AbstractTwo.java
+++ src/main/java/com/example/jaoo/m3/s6/AbstractTwo.java
@@ -3,7 +3,7 @@
  * 
  * https://github.com/egalli64/jaoo
  */
-package com.example.jaoo.m3.s5;
+package com.example.jaoo.m3.s6;
 
 /**
  * An abstract class. Having no abstract method it could also be concrete. We
